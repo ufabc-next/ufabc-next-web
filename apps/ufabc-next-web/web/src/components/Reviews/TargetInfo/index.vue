6091163f3bba12c105a450913c9b8b6f28ca77bf
Felipe Silva <felipeo.silva@hotmail.com>
2019-03-12 03:29:04 -0300

    Test with facebook login in homepage and finish first layout of subject page

--- apps/ufabc-next-web/web/src/components/Reviews/TargetInfo/index.vue
+++ apps/ufabc-next-web/web/src/components/Reviews/TargetInfo/index.vue
@@ -6,17 +6,22 @@
     <v-flex xs12 :style="style"  v-if='commentsCount >= 0 && target.kind == "teacher"' style="display: flex;">
       <v-icon size="20" class="mr-1" color="grey">mdi-message-draw</v-icon> {{ commentsCount }} comentários
     </v-flex>
-    <v-flex xs12 :style="style"  style="display: flex;">
+    <v-flex xs12 :style="style" v-if='target.kind == "teacher"' style="display: flex;">
       <v-icon size="20" class="mr-1" color="grey">mdi-gmail</v-icon> 
       <a href="mailto:professor@ufabc.edu.br" style="text-decoration: none;">
         professor@ufabc.edu.br
       </a>
     </v-flex>
-    <v-flex xs12 :style="style"  style="display: flex;">
+    <v-flex xs12 :style="style"  style="display: flex;" v-if='target.kind == "teacher"'>
       <v-icon size="20" class="mr-1" color="grey">mdi-home</v-icon> Sala 1035 (Bloco B)
     </v-flex>
-    <v-flex xs12 :style="style"  style="display: flex;">
+    <v-flex xs12 :style="style"  style="display: flex;" v-if='target.kind == "teacher"'>
       <v-icon size="20" class="mr-1" color="grey">mdi-phone</v-icon> (11) 4444-4444
+    </v-flex>
+
+    <!-- Subject -->
+    <v-flex xs12 :style="style" v-if='teachersCount >= 0 && target.kind == "subject"' style="display: flex;">
+      <v-icon size="20" class="mr-1" color="grey">mdi-account</v-icon> {{ teachersCount }} professores
     </v-flex>
   </v-layout>
 </template>
@@ -37,6 +42,10 @@ export default {
 
     target: {
       type: Object
+    },
+
+    teachersCount: {
+      type: Number
     }
   },
 
