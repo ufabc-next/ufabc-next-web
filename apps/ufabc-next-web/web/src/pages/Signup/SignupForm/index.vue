fbe28d66b1f41f5cc689d083ba4b59d586a00165
lucas-grippa <lucas-grippa@hotmail.com>
2019-03-15 11:20:50 -0300

    Working on stats

---
+++ apps/ufabc-next-web/web/src/pages/Signup/SignupForm/index.vue
@@ -0,0 +1,139 @@
+<template>
+<div style="background-color: white; height: 100%; width: 100%;">
+  <v-container column style="background-color: white; height: 100%; width: 100%;">
+    <v-layout row wrap style="width: 100%; flex: none">
+      <v-flex xs3 sm1 md2 lg2>
+        <img style="max-width: 100%; height: auto;" src="@/assets/logo.svg" />
+      </v-flex>
+      <v-flex sm5 md5 lg5></v-flex>
+      <v-flex sm5 md5 lg5>
+        <el-steps :active="currentStep" finish-status="success">
+          <el-step title="Passo 1"></el-step>
+          <el-step title="Passo 2"></el-step>
+          <el-step title="Passo 3"></el-step>
+        </el-steps>
+      </v-flex>
+    </v-layout>
+    <v-layout align-center row wrap style="width: 100%; flex: 1 1 auto;">
+        <!-- Art -->
+        <v-flex sm12 md7 lg7>
+          <img style="max-width: 100%; height: auto;" src="@/assets/signup.svg" />
+        </v-flex>
+
+        <!-- Steps -->
+        <v-flex sm12 md5 lg5>
+            <!-- Step 1 -->
+            <v-layout column fill-width v-if="currentStep == 0">
+              <div class="mb-4">
+                <v-layout wrap>
+                <span class="step-title">O que você faz na UFABC?</span>
+                </v-layout>         
+                <v-layout wrap>
+                <span class="step-subtitle">Selecione uma das opções:</span>
+                </v-layout>
+              </div>
+              <v-layout fill-height>
+                <!-- Student -->
+                <v-layout class="elevate-3d pa-4 border cursor-pointer mr-3" style="max-width: 160px;" @click="selectRole('student')">
+                  <v-icon class="mr-2">mdi-school</v-icon>
+                  <div>Aluno</div>
+                </v-layout>
+                <!-- Teacher -->
+                <v-layout align-center class="elevate-3d pa-4 border cursor-pointer" style="max-width: 160px;" @click="selectRole('teacher')">
+                  <v-icon class="mr-2">mdi-account</v-icon>
+                  <div>Professor</div>
+                </v-layout>
+              </v-layout>
+            </v-layout>
+
+            <!-- Step 2 -->
+            <v-layout column fill-width pr-4 v-if="currentStep == 1">
+              <div v-if="role == 'student'">
+                <div class="step-title mb-4">
+                  Falta pouco para completar o seu cadastro
+                </div>
+                <div class="step-subtitle mb-2">Insira seu email institucional</div>
+                <v-text-field
+                  placeholder="Ex: next@aluno.ufabc.edu.br"
+                  solo
+                ></v-text-field>
+                <div class="step-subtitle mb-2">Insira seu RA</div>
+                <v-text-field
+                  placeholder="Ex: 11012014"
+                  solo
+                ></v-text-field>
+                <div class="step-subtitle mb-2">Confirme seu RA</div>
+                <v-text-field
+                  placeholder="Ex: 11012014"
+                  solo
+                ></v-text-field>
+              </div>
+              <div v-if="role == 'teacher'">
+                Professor
+              </div>
+            </v-layout>
+
+            <!-- Step 3 -->
+            <div v-if="currentStep == 2">
+              Step 3
+            </div>
+        </v-flex>
+      </v-layout>
+
+      <v-layout align-end row style="width: 100%; flex: none; height:57px;" >
+        <v-flex sm7 md7 lg7>
+        </v-flex>
+        <v-flex v-show="currentStep != 0" sm5 md5 lg5 class="border-top" style="display: flex;">
+          <v-btn flat round large class="mr-2" @click="back()">
+            < Anterior
+          </v-btn>
+          <v-flex></v-flex>
+          <v-btn style="background-color: #00EB5E; color: white;" round large @click="next()">
+            Próximo >
+          </v-btn>
+        </v-flex>
+      </v-layout>
+  </v-container>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'SignupForm',
+  data() {
+    return {
+      currentStep: 0,
+      role: '',
+    };
+  },
+
+  methods: {
+      next() {
+        if (this.currentStep++ > 2) this.currentStep = 0;
+      },
+
+      back() {
+        if (this.currentStep-- < 0) this.currentStep = 0;
+      },
+
+      selectRole(role) {
+        this.role = role
+        this.next()
+      }
+    }
+
+}
+</script>
+
+<style scoped>
+.step-title {
+  font-size: 26px;
+  font-weight: bold;
+}
+
+.step-subtitle {
+  font-size: 14px;
+  color: rgba(0,0,0,0.5);
+}
+
+</style>
--- apps/ufabc-next-web/web/src/pages/Stats/Stats.js
+++ apps/ufabc-next-web/web/src/pages/Stats/Stats.js
@@ -1,38 +1,168 @@
 import _ from 'lodash'
+import VueHighcharts from 'vue2-highcharts'
+import Highcharts3D from "highcharts/highcharts-3d";
+import Highcharts from "highcharts";
+
 import Stats from '@/services/Stats'
 
 export default {
   name: 'Stats',
-
+  components: {
+    VueHighcharts,
+  },
   data() {
-    this.chartSettings = {
-      area: true
-    }
-    return {
-      studentId: '11012014',
-      statsLoading: false,
-      chartData: {
-        columns: ['season', 'cr'],
-        rows: [
-          // { 'cr': 2.56, 'season': '01/01',},
-          // { 'cr': 2.60, 'season': '01/02',},
-          // { 'cr': 2.90, 'season': '01/03',},
-          // { 'cr': 3.19, 'season': '01/04',},
-          // { 'cr': 2.12, 'season': '01/05',},
-          // { 'cr': 2.34, 'season': '01/06',}
-        ]
+    this.crHistorySettings = {
+      area: true,
+      areaStyle: {
+        color: {
+          type: 'linear',
+          x: 0,
+          y: 0,
+          x2: 1,
+          y2: 0,
+          colorStops: [{
+              offset: 0, color: '#8E2DE2' // color at 0% position
+          }, {
+              offset: 1, color: '#4A00E0' // color at 100% position
+          }],
+          globa: false // false by default
+        }
       },
 
-      chartData2: {
-          columns: ['crInterval', 'count',],
-          rows: [
-            { 'count': 300, 'crInterval': '0.0 <= cr < 1.0', },
-            { 'count': 600, 'crInterval': '1.0 <= cr < 2.0', },
-            { 'count': 2000, 'crInterval': '2.0 <= cr < 3.0', },
-            { 'count': 200, 'crInterval': '3.0 <= cr <= 4.0', },
-          ]
+      lineStyle: {
+        color: {
+            type: 'linear',
+            x: 0,
+            y: 0,
+            x2: 1,
+            y2: 0,
+            colorStops: [{
+                offset: 0, color: '#8E2DE2' // color at 0% position
+            }, {
+                offset: 1, color: '#4A00E0' // color at 100% position
+            }],
+            globa: false // false by default
         }
-    } 
+      },
+
+      itemStyle: {
+        color: {
+            type: 'linear',
+            x: 0,
+            y: 0,
+            x2: 1,
+            y2: 0,
+            colorStops: [{
+                offset: 0, color: '#8E2DE2' // color at 0% position
+            }, {
+                offset: 1, color: '#4A00E0' // color at 100% position
+            }],
+            globa: false // false by default
+        }
+      }
+    }
+    return {
+      Highcharts,
+      studentId: '11012014',
+      crHistoryLoading: false,
+      crDistributionLoading: false,
+      crHistoryOptions: {
+        columns: ['season', 'cr'],
+        rows: []
+      },
+      crDistributionOptions: {
+        chart: {
+          type: 'area',
+          zoomType: 'x',
+          panning: true,
+          panKey: 'shift',
+          
+        },
+
+        responsive: {
+          rules: [{
+            condition: {
+              maxWidth: 1200
+            },
+            chartOptions: {
+              legend: {
+                  layout: 'horizontal',
+                  align: 'center',
+                  verticalAlign: 'bottom'
+              }
+            }
+          }]
+        },
+
+        title: {
+          text: ''
+        },
+
+        subtitle: {
+          text: ''
+        },
+
+        annotations: [{
+        labelOptions: {
+          backgroundColor: 'rgba(255,255,255,1)',
+          verticalAlign: 'top',
+          y: 15
+        },
+        labels: [{
+          point: {
+              xAxis: 0,
+              yAxis: 0,
+              x: 27.98,
+              y: 50
+          },
+          text: 'Arbois'
+        }]
+      }],
+
+        xAxis: {
+          labels: {
+            format: '{value}'
+          },
+          minRange: 4,
+          title: {
+            text: 'CR'
+          }
+        },
+
+        yAxis: {
+          startOnTick: true,
+          endOnTick: false,
+          maxPadding: 0.35,
+          title: {
+            text: null
+          },
+          labels: {
+            format: '{value}'
+          }
+        },
+
+        tooltip: {
+          headerFormat: '{point.x:.1f}<br>',
+          shared: true
+        },
+
+        legend: {
+          enabled: false
+        },
+
+        series: [{
+          data: [],
+          lineColor: Highcharts.getOptions().colors[1],
+          color: Highcharts.getOptions().colors[2],
+          fillOpacity: 0.5,
+          name: 'Número de alunos',
+          marker: {
+            enabled: false
+          },
+          threshold: null
+        }]
+      }
+    }
   },
 
   created() {
@@ -44,24 +174,47 @@ export default {
   },
 
   methods: {
+    fetch() {
+      this.populateCrHistory()
+      this.populateCrDistribution()    
+    },
 
-    async fetch() {
-      this.statsLoading = true
+    async populateCrHistory() {
+      this.crHistoryLoading = true
       try {
-        let res = await Stats.getCrHistory(this.studentId)
-        if(!res) return
+        let crHistoryData = await Stats.getCrHistory(this.studentId)
+        if(!crHistoryData) return
 
-        this.chartData.rows = res
-        this.statsLoading = false
-        
+        this.crHistoryOptions.rows = crHistoryData
+        this.crHistoryLoading = false
       } catch(err) {
         this.statsLoading = false
-        // this.$message({
-        //   type: 'error',
-        //   message: ErrorMessage(err),
-        // })
       }
-    }
+    },
+
+    async populateCrDistribution() {
+      this.crDistributionLoading = true
+
+      let maxWidth = 420
+      let maxHeight = 280
+      let onlyXs = this.$vuetify.breakpoint.xsOnly
+      let screenWidth = this.$vuetify.breakpoint.width
+      let width  =  onlyXs ? (screenWidth - 40) > maxWidth ? maxWidth : (screenWidth - 40) : maxWidth
+      let height =  onlyXs ? (screenWidth - 140) > maxHeight ? maxHeight : (screenWidth - 140) : maxHeight
+
+      try {
+        let crDistributionData = await Stats.getCrDistribution()
+        if(!crDistributionData) return
+
+        crDistributionData.map((el) => {
+          this.crDistributionOptions.series[0].data.push(el)  
+        })
+
+        this.crDistributionLoading = false
+      } catch(err) {
+        this.crDistributionLoading = false
+      }
+    },
   },
 
 }
