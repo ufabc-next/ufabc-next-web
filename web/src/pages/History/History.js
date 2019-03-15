66f3a2661eabedab0ea4d447f62ebf393afadb69
Felipe Silva <felipeo.silva@hotmail.com>
2019-03-14 23:41:50 -0300

    Finish initial version of historic

--- web/src/pages/History/History.js
+++ web/src/pages/History/History.js
@@ -1,3 +1,78 @@
+import History from '@/services/History'
+import ErrorMessage from '@/helpers/ErrorMessage'
+import _ from 'lodash'
+
 export default {
-  name: 'History'
+  name: 'History',
+
+  data(){
+    return {
+      history: null,
+      conceptsColor: {
+        'A': 'rgb(63, 207, 140)',
+        'B': 'rgb(184, 233, 134)',
+        'C': 'rgb(248, 183, 76)',
+        'D': 'rgb(255, 160, 4)',
+        'F': 'rgb(249, 84, 105)',
+        'O': 'rgb(169, 169, 169)',
+
+        // exceptions
+        'I': 'rgb(25, 118, 210)',
+        'E': 'rgb(25, 118, 210)',
+        'null': 'rgb(0, 0, 0)',
+      }
+    }
+  },
+
+  created() {
+    this.fetch()
+  },
+
+  computed: {
+    historyBySeason() {
+      if(this.history && this.history.disciplinas && this.history.disciplinas.length) {
+        return _.groupBy(this.history.disciplinas, 'quad')
+      }
+    },
+
+    historySeasons() {
+      return _.keys(this.historyBySeason)
+    },
+
+    subjectsCount() {
+      return _.get(this.history, 'disciplinas.length', 0)
+    }
+  },
+
+  methods: {
+    seasonLabel(season) {
+      let seasonSplited = season.split(':')
+      if(seasonSplited.length < 2) return ''
+      return seasonSplited[1] + ' de ' + seasonSplited[0]
+    },
+
+    async fetch() {
+      this.loading = true
+
+      try {
+        let res = await History.get()
+
+        this.loading = false
+        if(res.data){
+          this.history = res.data
+
+          this.history.disciplinas.map((h) => {
+            h.quad = h.ano + ':' + h.periodo
+            return h
+          })
+        }
+      } catch(err) {
+        this.loading = false
+        this.$message({
+          type: 'error',
+          message: ErrorMessage(err),
+        }) 
+      }
+    },
+  }
 }
