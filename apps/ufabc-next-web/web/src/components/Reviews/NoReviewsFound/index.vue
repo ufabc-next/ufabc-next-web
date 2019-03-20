8ef8babf9c48ff50fab9dacca744361cf86150cb
Felipe Silva <felipeo.silva@hotmail.com>
2019-03-20 10:16:27 -0300

    Add Use Report, Settings page

--- apps/ufabc-next-web/web/src/components/Reviews/NoReviewsFound/index.vue
+++ apps/ufabc-next-web/web/src/components/Reviews/NoReviewsFound/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-layout class="text-xs-center" column align-center justify-center>
     Nenhum dado encontrado. Quer adicionar informações?
-    <v-btn color="info">Contribuir</v-btn>
+    <v-btn color="info">Adicionar</v-btn>
   </v-layout>
 </template>
 
--- apps/ufabc-next-web/web/src/menus/index.js
+++ apps/ufabc-next-web/web/src/menus/index.js
@@ -4,35 +4,37 @@ export default [
     title: 'Reviews',
     icon: 'mdi-message-draw',
     route: '/reviews',
-    permissions: 'reviews:read',
   },
 
   {
     title: 'Estatísticas',
     icon: 'mdi-google-analytics',
     route: '/stats',
-    permissions: 'stats:read',
   },
 
   {
     title: 'Planejamento',
     icon: 'mdi-file-document-box-multiple',
     route: '/planning',
-    permissions: 'planning:read',
   },
 
   {
     title: 'Meus histórico',
     icon: 'mdi-history',
     route: '/history',
-    permissions: 'history:read',
   },
-
 
   {
     title: 'Configurações',
     icon: 'settings',
     route: '/settings',
+  },
+
+  {
+    title: 'Administrativo',
+    icon: 'mdi-database-edit',
+    route: '/admin',
+    permissions: 'admin',
   },
 
 ]
