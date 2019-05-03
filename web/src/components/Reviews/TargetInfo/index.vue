8beaeff3c3baf4f26c6d7fe78c6e0d52a5bb9457
Felipe Silva <felipeo.silva@hotmail.com>
2019-05-03 01:22:17 -0300

    hot fix

--- web/src/components/Reviews/TargetInfo/index.vue
+++ web/src/components/Reviews/TargetInfo/index.vue
@@ -1,12 +1,12 @@
 <template>
-  <v-layout class="target-info mb-3" wrap>
-    <div class="chipzinha" xs12 :style="style"  v-if='conceptsCount != null & conceptsCount >= 0' style="display: flex;">
+  <v-layout class="target-info" wrap style="display: flex; flex-direction: column; align-items: start;" :class="$vuetify.breakpoint.xsOnly ? 'mb-0' : 'mb-3'">
+    <div class="chipzinha" xs12 :style="style"  v-if='conceptsCount != null & conceptsCount >= 0'>
       <v-icon size="20" class="mr-1" color="inherit">mdi-format-annotation-plus</v-icon> {{ conceptsCount }} conceitos
     </div>
-    <div class="chipzinha" xs12 :style="style"  v-if='commentsCount >= 0 && target.kind == "teacher"' style="display: flex;">
+    <div class="chipzinha" xs12 :style="style"  v-if='commentsCount >= 0 && target.kind == "teacher"'>
       <v-icon size="20" class="mr-1" color="inherit">mdi-message-text-outline</v-icon> {{ commentsCount }} {{ commentsCount == '1' ? 'comentário' : 'comentários' }}
     </div>
-    <div class="chipzinha" xs12 :style="style"  v-if='classesCount >= 0 && target.kind == "teacher"' style="display: flex;">
+    <div class="chipzinha" xs12 :style="style"  v-if='classesCount >= 0 && target.kind == "teacher"'>
       <v-icon size="20" class="mr-1" color="inherit">mdi-teach</v-icon> {{ classesCount }} {{ classesCount == '1' ? 'disciplina' : 'disciplinas' }}
     </div><!-- 
     <div xs12 :style="style" v-if='target.kind == "teacher"' style="display: flex;">
@@ -74,5 +74,7 @@ export default {
   padding: 4px 8px;
   border-radius: 16px;
   color: var(--ufabcnext-blue);
+  display: inline-flex;
+  margin-bottom: 8px;
 }
 </style>
