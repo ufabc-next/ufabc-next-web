fd5a05cfe41163906f64310d4a08fd769d6e397e
Felipe Silva <felipeo.silva@hotmail.com>
2019-08-07 20:09:23 -0300

    Add cyto-scape

--- web/src/pages/Relationship/Relationship.js
+++ web/src/pages/Relationship/Relationship.js
@@ -1,5 +1,8 @@
 import User from '@/services/User'
 import ErrorMessage from '@/helpers/ErrorMessage'
+import cytoscape from 'cytoscape'
+import cola from 'cytoscape-cola'
+cytoscape.use(cola)
 
 export default {
   name: 'Relationship',
@@ -7,6 +10,7 @@ export default {
   data() {
     return {
       loading: false,
+      nodes: null,
     }
   },
 
@@ -19,14 +23,58 @@ export default {
   },
 
   methods: {
+    graphSettings(nodes, edges){
+      cytoscape({
+        container: document.getElementById('cy'),
+        autounselectify: true,
+        boxSelectionEnabled: false,
+        layout: {
+          name: 'cola',
+          maxSimulationTime: 30000,
+          edgeLength: function(edge) { return 350 },
+        },
+        style: [
+          {
+            selector: 'node',
+            css: {
+              'background-color': 'data(color)',
+              'label': "data(id)",
+              'font-weight': 'bold',
+              'font-size': '45px',
+              'width': 80,
+              'height': 80,
+            }
+          },
+          {
+            selector: 'edge',
+            css: {
+              'label': "data(recurrence)",
+              'width': 'data(width)',
+              'line-color': '#4267b2',
+              'opacity': 0.4,
+              'font-size': '60px',
+              'font-weight': 'bold'
+            }
+          }
+        ],
+        elements: {
+          nodes: nodes,
+          edges: edges
+        }
+      })
+    },
+
     async fetch(){
       try {
         this.loading = true
         let res = await User.relationships()
 
         this.loading = false
-        console.log(res)
+        let nodes = res.data.nodes
+        let edges = res.data.edges
+        this.graphSettings(nodes, edges)
       } catch(err) {
+        console.log(err)
         this.loading = false
         this.$message({
           type: 'error',
