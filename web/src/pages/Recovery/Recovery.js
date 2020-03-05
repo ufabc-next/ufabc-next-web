60fc03655dad6e1e434d77f00e11132c13f806c3
FeoSilva <=>
2020-03-05 02:08:51 -0300

    Finish UI of recovery

--- web/src/pages/Recovery/Recovery.js
+++ web/src/pages/Recovery/Recovery.js
@@ -1,13 +1,40 @@
 import Auth from '@/services/Auth'
+import User from '@/services/User'
+import ErrorMessage from '@/helpers/ErrorMessage'
+
 export default {
   name: 'Recovery',
 
-  created() {
-
-  },
   data() {
     return {
-
+      email: null,
+      notDefaultEmail: false,
+      wasRecovered: false,
+      loading: false,
+      emailSuffix: '@aluno.ufabc.edu.br',
     }
+  },
+  methods: {
+    async next() {
+      if(!this.notDefaultEmail) {
+        this.email = this.email + this.emailSuffix
+      }
+      try {
+        this.loading = true
+        let res = await User.recovery({ email: this.email })
+        this.wasRecovered = true
+        this.loading = false
+      } catch(err) {
+        this.loading = false
+
+        this.$message({
+          type: 'error',
+          message: ErrorMessage(err),
+        })
+      }
+    },
+    openUFABCMail() {
+      window.open('https://www.outlook.com/aluno.ufabc.edu.br', '_blank')
+    },
   },
 }
