34bd6180b2efa6c5be1382293d20ca369d9ac2e9
arthurlimadossantos <47360021+arthurlimadossantos@users.noreply.github.com>
2021-10-13 20:58:56 -0300

    changed commands to run local

--- apps/ufabc-next-web/web/src/pages/Register/Register.js
+++ apps/ufabc-next-web/web/src/pages/Register/Register.js
@@ -13,15 +13,17 @@ export default {
   },
   methods: {
     loginFacebook() {
+      const env = process.env.NODE_ENV
       window.open(
-        (`${process.env.VUE_APP_API_URL}/connect/facebook?inApp=${this.inApp}`).replace('/v1', ''),
+        (`${process.env.VUE_APP_API_URL}/connect/facebook?inApp=${this.inApp}&env=${env}`).replace('/v1', ''),
         this.inApp ? '_system' : '_self'
       );
     },
 
     loginGoogle() {
+      const env = process.env.NODE_ENV
       window.open(
-        (`${process.env.VUE_APP_API_URL}/connect/google?inApp=${this.inApp}`).replace('/v1', ''),
+        (`${process.env.VUE_APP_API_URL}/connect/google?inApp=${this.inApp}&env=${env}`).replace('/v1', ''),
         this.inApp ? '_system' : '_self'
       );
     },
