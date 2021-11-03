43f98e97761f07301c6a23fd185399ff564bc03a
Vinicius Reis <vinecreis@gmail.com>
2021-11-03 19:37:30 -0300

    Removing WhatsApp page files, adding the docs`link to the new button

--- apps/ufabc-next-web/web/src/pages/WhatsApp/index.vue
+++
@@ -1,3 +0,0 @@
-<template src="./WhatsApp.html"></template>
-<script src="./WhatsApp.js"></script>
-<style src="./WhatsApp.css"></style>
--- apps/ufabc-next-web/web/src/router/index.js
+++ apps/ufabc-next-web/web/src/router/index.js
@@ -15,7 +15,6 @@ import Stats from '@/pages/Stats'
 import Admin from '@/pages/Admin'
 import Relationship from '@/pages/Relationship'
 import Recovery from '@/pages/Recovery'
-import WhatsApp from '@/pages/WhatsApp'
 
 import Auth from '@/services/Auth'
 
@@ -192,14 +191,6 @@ const router = new VueRouter({
       name: 'register',
       path: '/register',
       component: Register
-    },
-    {
-      name: 'whatsapp',
-      path: '/whatsapp',
-      component: WhatsApp,
-      meta: {
-        title: 'Grupos no WhatsApp'
-      },
     },
 
     // { path: '*', redirect: '/login' }
