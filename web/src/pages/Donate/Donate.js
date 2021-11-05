f1fd1ac32f52617e73ef859d70fa423dfbe6f932
Gustavo Ueti <gustavo.ueti@gmail.com>
2021-11-05 20:40:42 -0300

    feat: :construction: add inicial dialog

--- web/src/pages/Donate/Donate.js
+++ web/src/pages/Donate/Donate.js
@@ -1,13 +1,26 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
+
 export default {
     name: 'Donate',
+    created(){
+      console.log(this)
+    },
     methods: {
-        async teste() {
+        async showModal() {
             await this.$dialog({
-              title: 'teste',
-              html: `teste do dinheiro`,
+              title: 'Informações da conta:',
+              html: `
+              <img src="./images/pix-fe.png" alt="QRCode Pix" aria-label="QRCode da chave pix">
+              <p> Nome: Felipe Oliveira Silva </p>
+              <p> Chave Pix: 4e031758-52fb-4c9c-876c-894dcaa6afe9 </p>
+              <a href="https://nubank.com.br/pagar/1b0zby/HVkAkLCsHr">
+                <p> Clique e contribua!</p>
+              </a>
+              `,
               buttons: [
-                {  name: 'botao 1', class: 'red--text'},
-                {  name: 'botao 2', action: true, class: 'green--text'}
+                {  name: 'Voltar', class: 'ufabcnext-grey--text'},
+                {  name: 'Qqr', class: 'red--text'},
               ]
             })
         }
