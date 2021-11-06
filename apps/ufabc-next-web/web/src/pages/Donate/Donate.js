79d28e7aa1da0c065d9211437c4ace99f2a5c67f
Bruna Ishida <bruishida2107@gmail.com>
2021-11-06 03:21:03 -0300

    add: donate dialog

--- apps/ufabc-next-web/web/src/pages/Donate/Donate.js
+++ apps/ufabc-next-web/web/src/pages/Donate/Donate.js
@@ -1,28 +1,12 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
-
 export default {
-    name: 'Donate',
-    created(){
-      console.log(this)
-    },
-    methods: {
-        async showModal() {
-            await this.$dialog({
-              title: 'Informações da conta:',
-              html: `
-              <img src="./images/pix-fe.png" alt="QRCode Pix" aria-label="QRCode da chave pix">
-              <p> Nome: Felipe Oliveira Silva </p>
-              <p> Chave Pix: 4e031758-52fb-4c9c-876c-894dcaa6afe9 </p>
-              <a href="https://nubank.com.br/pagar/1b0zby/HVkAkLCsHr">
-                <p> Clique e contribua!</p>
-              </a>
-              `,
-              buttons: [
-                {  name: 'Voltar', class: 'ufabcnext-grey--text'},
-                {  name: 'Qqr', class: 'red--text'},
-              ]
-            })
-        }
-    }   
-}
+  name: "Donate",
+  data() {
+    return {
+      dialog: false,
+    };
+  },
+  created() {
+    console.log(this);
+  },
+  methods: {},
+};
