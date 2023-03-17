c14bbcbe898e2de582cf15a9efc2481e9075db22
FusiDaniel <fpires.daniel@gmail.com>
2023-03-17 02:10:41 -0300

    chore: add vuetify

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,15 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-createApp(App).use(router).mount("#app");
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+createApp(App).use(router).use(vuetify).mount("#app");
