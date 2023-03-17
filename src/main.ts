88054a3dbc1fea9da187c75dc7b05aada3b87bf4
FusiDaniel <fpires.daniel@gmail.com>
2023-03-17 03:00:20 -0300

    fix: linting

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-import "vuetify/styles";
-import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
 const vuetify = createVuetify({
   components,
   directives,
 });
 
-import elementPlus from "element-plus";
-import "element-plus/dist/index.css";
+import elementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 
-createApp(App).use(router).use(vuetify).use(elementPlus).mount("#app");
+createApp(App).use(router).use(vuetify).use(elementPlus).mount('#app');
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,11 +6,11 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { defineComponent } from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
-  name: "HomeView",
+  name: 'HomeView',
   components: {
     HelloWorld,
   },
