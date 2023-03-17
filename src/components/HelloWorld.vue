be7add0aaf0979dd4e2111e6ec9aeffe80451739
FusiDaniel <fpires.daniel@gmail.com>
2023-03-17 03:00:20 -0300

    fix: linting

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -111,10 +111,10 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: "HelloWorld",
+  name: 'HelloWorld',
   props: {
     msg: String,
   },
