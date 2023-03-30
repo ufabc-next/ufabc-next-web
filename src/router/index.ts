342954bbe3aedbca759d0939ab1b1bf5f6b8e391
FusiDaniel <fpires.daniel@gmail.com>
2023-03-30 12:21:46 -0300

    feat: basic navbar

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,13 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import ReviewsView from '../views/ReviewsView.vue';
+import PerformanceView from '../views/PerformanceView.vue';
+import RelationshipView from '../views/RelationshipView.vue';
+import PlanningView from '../views/PlanningView.vue';
+import HistoryView from '../views/HistoryView.vue';
+import StatsView from '../views/StatsView.vue';
+import SettingsView from '../views/SettingsView.vue';
+import DonateView from '../views/DonateView.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -8,14 +16,55 @@ const routes: Array<RouteRecordRaw> = [
     component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    path: '/reviews',
+    name: 'reviews',
+    component: ReviewsView,
   },
+  {
+    path: '/performance',
+    name: 'performance',
+    component: PerformanceView,
+  },
+  {
+    path: '/relationship',
+    name: 'relationship',
+    component: RelationshipView,
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    component: PlanningView,
+  },
+  {
+    path: '/history',
+    name: 'history',
+    component: HistoryView,
+  },
+  {
+    path: '/stats',
+    name: 'stats',
+    component: StatsView,
+  },
+  {
+    path: '/settings',
+    name: 'settings',
+    component: SettingsView,
+  },
+  {
+    path: '/donate',
+    name: 'donate',
+    component: DonateView,
+  },
+
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+  // },
 ];
 
 const router = createRouter({
