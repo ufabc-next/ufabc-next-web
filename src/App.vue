0fdecdf19de649666dd617119b60e6bffbc36cd8
FusiDaniel <fpires.daniel@gmail.com>
2023-03-30 12:21:46 -0300

    feat: basic navbar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <v-app>
+    <AppBar />
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import { useTheme } from 'vuetify';
+import AppBar from '@/components/AppBar.vue';
 
-nav {
-  padding: 30px;
+export default {
+  setup() {
+    const theme = useTheme();
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+    return { theme };
+  },
+  components: {
+    AppBar,
+  },
+};
+</script>
