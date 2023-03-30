be9d0054663431f7045b8298121ee695c39a7837
Renan Zago Lorijola <renanzago@gmail.com>
2023-03-30 01:23:38 -0300

    chore: configure axios instance to call apis

---
+++ src/environment.ts
@@ -0,0 +1,15 @@
+const apiVersion = 'v1';
+
+export const API_URL = (() => {
+  const URL = window.location.href;
+
+  if (URL.includes('localhost')) {
+    return `http://localhost:8011/${apiVersion}`;
+  }
+
+  if (URL.includes('192.168')) {
+    return URL.replace(/:\d+.*/, ':8011/v1');
+  }
+
+  return `https://api.ufabcnext.com/${apiVersion}`;
+})();
