1c2eafffd0ff9926c2d6565c38e48ebc5df18648
Renan Zago Lorijola <renanzago@gmail.com>
2023-03-30 00:23:12 -0300

    chore: lint staged, jest and license config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,18 @@
-module.exports = {
+/** @type {import('jest').Config} */
+const config = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{js,vue}'],
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  coverageReporters: ['lcov'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
+
+module.exports = config;
