8fa52c3f9e8beef819a698e6b81633c9840d1075
FusiDaniel <fpires.daniel@gmail.com>
2023-03-30 12:22:15 -0300

    feat: routes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,5 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <h1>Home</h1>
   </div>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
-</script>
