18c0c84f708f4d0c5c38d739c430eaa8cda2539e
FusiDaniel <fpires.daniel@gmail.com>
2023-03-31 12:46:05 -0300

    feat: reviews welcome

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -3,7 +3,9 @@
     <v-list>
       <v-list-item
         v-for="item in NavigationsItems"
-        :href="item.route"
+        :href="item.route || item.url"
+        :target="item.url && '_blank'"
+        :rel="item.url && 'noopener noreferrer'"
         :key="item.title"
         :prepend-icon="item.icon"
       >
@@ -12,12 +14,13 @@
     </v-list>
   </v-navigation-drawer>
 
-  <v-app-bar app height="min-content" class="py-2">
+  <v-app-bar elevation="1" app height="min-content" class="py-2">
     <v-app-bar-nav-icon
       app
       variant="text"
       color="primary"
       @click.stop="drawer = !drawer"
+      class="d-lg-none"
     ></v-app-bar-nav-icon>
 
     <v-spacer></v-spacer>
@@ -26,7 +29,34 @@
 
     <v-spacer></v-spacer>
 
-    <v-btn variant="text" icon="mdi-dots-vertical" color="primary"></v-btn>
+    <v-btn color="primary" icon="mdi-dots-vertical">
+      <v-icon></v-icon>
+      <v-menu activator="parent">
+        <v-list class="px-2">
+          <v-list-item>
+            <v-layout>
+              <v-avatar :size="38" color="primary">
+                {{ userInitials }}
+              </v-avatar>
+              <v-layout class="flex-column ml-4">
+                <p>{{ userLogin }}</p>
+                <p v-if="user.ra" class="text-caption text-medium-emphasis">
+                  RA: {{ user.ra }}
+                </p>
+              </v-layout>
+            </v-layout>
+          </v-list-item>
+          <v-list-item>
+            <v-btn
+              prepend-icon="mdi-exit-to-app"
+              variant="text"
+              class="text-capitalize text-body-2"
+              >Sair</v-btn
+            >
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-btn>
   </v-app-bar>
   <div style="height: 64px" />
 </template>
@@ -38,6 +68,17 @@ export default {
   computed: {
     NavigationsItems() {
       return NavigationsItems;
+    },
+    userLogin() {
+      return 'fusimoto.daniel';
+    },
+    userInitials() {
+      return 'fd';
+    },
+    user() {
+      return {
+        ra: '123456',
+      };
     },
   },
   data: () => ({
