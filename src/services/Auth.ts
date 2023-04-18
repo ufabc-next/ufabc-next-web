351c1e5b4cd93904ef2ba631cad603bac3952c2a
Renan Zago Lorijola <renanzago@gmail.com>
2023-04-18 01:17:32 -0300

    feat: login dev
    Co-authored-by: Daniel Fusimoto Pires <FusiDaniel@users.noreply.github.com>

--- src/services/Auth.ts
+++ src/services/Auth.ts
@@ -1,195 +1,104 @@
-import Axios from 'axios';
+import axios from 'axios';
+import { ref } from 'vue';
 import JwtDecode from 'jwt-decode';
 
-interface Device {
-  _id: string;
-  deviceId: string;
-  token: string;
-  phone: string;
-}
-
-interface User {
-  _id: string;
-  oauth: {
-    email: string;
-    emailFacebook?: string;
-    emailGoogle?: string;
-    facebook?: string;
-    google?: string;
-    picture?: string;
-  };
-  confirmed: boolean;
-  email: string;
-  ra: number;
-  createdAt: Date;
-  devices?: Array<Device>;
-}
+import type { User } from '@/types';
 
 class Auth {
-  public user: User | null;
-  private _listeners: Array<(any: any) => void> | any; // receive functions in 'router/index.js' on original project
-  private token: string | null; // I don't know what kind of it here. I need to align with the backend
+  user = ref<User | null>(null);
+  token = ref<string | null>(null);
+  private _listeners: ((user: User) => void)[] = [];
 
   constructor() {
-    this.user = null;
-    this.token = null;
-    // -------------------------------------------
-    // O próprio vue já é reativo. Definir isso com ref() ou deixar padrão?
-    // Fonte: https://stackoverflow.com/questions/41791193/vuejs-reactive-binding-for-a-plugin-how-to
-    // comentei pq vue.util não existe no vue 3
-    // mas não sei o impacto disso no resto do projeto
-
-    // Vue.util.defineReactive(this, 'user', null);
-    // Vue.util.defineReactive(this, 'token', null);
-    // -------------------------------------------
     this.loadFromLocalStorage();
   }
 
-  /**
-   * system logout via token
-   */
-  public logOut(): void {
+  logOut() {
     this.setToken(null);
   }
 
-  /**
-   * get auth status
-   * @returns boolean
-   */
-  public isLoggedIn() {
+  isLoggedIn() {
     if (this.user) {
-      window &&
-        window.opener &&
-        window.opener.parent &&
-        window.opener.parent.postMessage(this.token, '*');
+      window?.opener?.parent?.postMessage(this.token.value, '*');
     }
     return !!this.user;
   }
 
-  /**
-   * set auth token
-   * @return - undefined if token pre-setted
-   */
-  public setToken(token: string | null) {
-    if (this.token === token) {
+  setToken(token: string | null) {
+    if (this.token.value === token) {
       return;
     }
 
-    // I modified this code snippet to be able to infer types more assertively.
-    // Please compare with the original code in services/auth.js
-    // in the master branch of the project
-    if (token) {
-      window &&
-        window.opener &&
-        window.opener.parent &&
-        window.opener.parent.postMessage(token, '*');
-
+    try {
+      if (token) {
+        window?.opener?.parent?.postMessage(token, '*');
+      }
+      if (!token) {
+        localStorage.removeItem('token');
+        this.token.value = null;
+        this.user.value = null;
+        return;
+      }
       localStorage.setItem('token', token);
-      this.user = JwtDecode(token);
-      this.token = token;
-    } else {
+      this.user.value = JwtDecode(token);
+      this.token.value = token;
+    } catch (e) {
       localStorage.removeItem('token');
-      this.token = null;
-      this.user = null;
+      this.token.value = null;
+      this.user.value = null;
     }
 
-    for (const k in this._listeners) {
-      const listener = this._listeners[k];
-      listener(this.user);
+    for (const listener of this._listeners) {
+      this.user.value && listener(this.user.value);
     }
   }
 
-  /**
-   * load token saved in localstorage (???) i dont understand this function
-   *
-   */
   loadFromLocalStorage() {
     const token = localStorage.getItem('token') || null;
     this.setToken(token);
   }
 
-  /**
-   * function used in 'router/index.js' on original project, but i dont understand the reason.
-   *
-   */
-  public onAuthStateChanged(
-    callback: <TReturn>(user?: User | null) => TReturn,
-  ) {
-    /**
-     * I'm having a problem with this line of code below.
-     * For some reason TS is pulling the "number|string|symbol"
-     * type from somewhere I still don't understand.
-     */
+  onAuthStateChanged(callback: (user: any) => void) {
+    if (!this._listeners.includes(callback)) {
+      this._listeners.push(callback);
+    }
 
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    if (!(callback in this._listeners)) this._listeners.push(callback);
-
-    if (this.user !== undefined) setTimeout(() => callback(this.user), 0);
+    if (this.user.value !== undefined) {
+      setTimeout(() => callback(this.user.value), 0);
+    }
   }
 
-  /**
-   * AJAX call to forgot
-   * @param email - user email
-   * @returns - AJAX request (axios)
-   */
-  public async forgot(email: string) {
-    return await Axios.get('/auth/reset', {
+  async forgot(email: string) {
+    return await axios.get('/auth/reset', {
       params: {
         email: email,
       },
     });
   }
 
-  /**
-   * AJAX call to reset
-   * @param user - i dont know what is
-   * @returns - AJAX request (axios)
-   */
-  public async reset(user: User) {
-    return await Axios.post('/auth/reset', user);
+  async reset(user: User) {
+    return await axios.post('/auth/reset', user);
   }
 
-  /**
-   * session device for mobile
-   */
-  public async addDevice() {
+  async addDevice() {
     const firebaseToken = localStorage.getItem('firebaseToken') || null;
-
-    // "window.device.uuid" returns error:
-    // Property 'device' does not exist on type 'Window & typeof globalThis'.ts(2339)
-
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const deviceId = window.device.uuid;
+    const deviceId = window.device?.uuid;
 
     if (this.isLoggedIn() && firebaseToken && deviceId) {
-      await Axios.post('/users/me/devices', {
+      await axios.post('/users/me/devices', {
         token: firebaseToken,
         deviceId: deviceId,
       });
     }
   }
 
-  public async removeDevice() {
-    // "window.device.uuid" returns error:
-    // Property 'device' does not exist on type 'Window & typeof globalThis'.ts(2339)
-
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const deviceId = window.device.uuid;
+  async removeDevice() {
+    const deviceId = window.device?.uuid;
 
     if (deviceId) {
-      return await Axios.delete(`/users/me/devices/${deviceId}`);
+      return await axios.delete(`/users/me/devices/${deviceId}`);
     }
   }
 }
 
-/**
- * When I got here, I had to put all the attributes I set in the constructor as optional
- * because TS asks for the required arguments.
- * I need help with this module.
- */
-// const teste = new Auth();
-// teste.user?.createdAt
 export default new Auth();
--- src/services/User.ts
+++ src/services/User.ts
@@ -7,17 +7,11 @@ class User {
   }
 
   async confirmSignup(params = {}) {
-    // return await Axios.post('/users/me/confirm', params)
     const token = await Axios.post('/account/confirm', params);
 
-    //---------------------------------
-    // problem with "window.device" again
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
     if (window.device) {
       await Auth.addDevice();
     }
-    // -----------------------------------
 
     return token;
   }
@@ -36,15 +30,6 @@ class User {
 
   async info() {
     return await Axios.get('/users/info');
-  }
-
-  async relationships(breadth: number, depth: number) {
-    return await Axios.get('/users/me/relationships', {
-      params: {
-        breadth: breadth,
-        depth: depth,
-      },
-    });
   }
 }
 
