edbbb897bb739ab5a0562f802e1ce3662a74573f
Mateus Braga <71946082+mateusbrg@users.noreply.github.com>
2023-05-10 23:21:24 -0300

    refactor: type axios errors correctly

--- src/helpers/ParseHTTPError.ts
+++ src/helpers/ParseHTTPError.ts
@@ -1,68 +1,64 @@
-// type ParseHTTPErrorParam = {
-//     response: Response  // e o que vem? KKKKKKKKKKKKKK
-// }
-// Workaround: We still don't know what type of response the backend sends
+import { AxiosError } from 'axios';
 
 /**
  * // Parses a error and returns it's name and message
  * @param err
  * @returns error type
  */
-export default function (err: any): Error | null {
+export default function (err: AxiosError<{ error: string }>) {
   // Its not an error
-  if (!err) {
-    return null;
-  }
+  // if (!err) {
+  //   return null;
+  // }
 
   // If it's an response wrapped in an error, unwrap it
-  const response = err.response ? err.response : err;
+  // const response = err.response ? err.response : err;
 
   // If not a valid object, return
-  if (!response) {
-    return err;
-  }
+  // if (!response) {
+  //   return err;
+  // }
 
   // Not a response object
-  if (!response.data) {
-    return err;
-  }
+  // if (!response.data) {
+  //   return err;
+  // }
   // If it has a Response object
-  const data = response.data;
-  if (data.type) {
+  // const data = response.data;
+
+  function typeForCode(code: string): string {
+    const HTTP_CODES: Record<string, string> = {
+      '400': 'BadRequest',
+      '401': 'Unauthorized',
+      '403': 'Forbidden',
+      '404': 'NotFound',
+      '409': 'Conflict',
+      '422': 'Unprocessable',
+      '500': 'InternalError',
+    };
+
+    return HTTP_CODES[code] || 'Error';
+  }
+
+  function BuildError(obj: { error: string; type: string; code?: string }) {
+    const error = new Error(obj.error);
+    error.name = obj.type || error.name;
+
+    Object.assign(error, obj);
+
+    return error;
+  }
+  if (err.response) {
+    const stringStatus = err.response.status.toString();
     return BuildError({
-      type: 'Error',
-      error: data.error,
-      // code: response.status
+      type: typeForCode(stringStatus),
+      error: err.response.data.error,
+      code: err.request.status,
+    });
+  } else {
+    return BuildError({
+      type: '500',
+      error: 'InternalError',
     });
   }
-
-  // Default is to return the
-  return BuildError({
-    type: typeForCode(data.status),
-    error: data,
-    code: data.status,
-  });
-}
-
-function typeForCode(code: string): string {
-  const HTTP_CODES: Record<string, string> = {
-    '400': 'BadRequest',
-    '401': 'Unauthorized',
-    '403': 'Forbidden',
-    '404': 'NotFound',
-    '409': 'Conflict',
-    '422': 'Unprocessable',
-    '500': 'InternalError',
-  };
-
-  return HTTP_CODES[code] || code;
-}
-
-function BuildError(obj: { error: string; type: string; code?: string }) {
-  const error = new Error(obj.error);
-  error.name = obj.type || error.name;
-
-  Object.assign(error, obj);
-
-  return error;
 }
