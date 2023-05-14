8bdb02870eb23e2d32e63dc56c591012766dc8c2
Mateus Braga <71946082+mateusbrg@users.noreply.github.com>
2023-05-14 06:31:24 -0300

    refactor: enviroments libs

--- src/environment.ts
+++ src/environment.ts
@@ -14,20 +14,20 @@
 //   return `https://api.ufabcnext.com/${apiVersion}`;
 // })();
 
-/**
- * The code above is "paused" because the original code has
- * 'HOME_URL' used in the registration page in the development environment.
- * For now, I've made occasional changes to make the "apiVersion" variable
- * compatible, but in the future this may also be rewritten
- *
- * I just kept the libraries because I have no idea of the impact of this
- * on the system as a whole, but I already suggest removal in
- * later features and rewriting in native apis or .env.{enviroment} file
- * like: https://cli.vuejs.org/guide/mode-and-env.html
- */
+// /**
+//  * The code above is "paused" because the original code has
+//  * 'HOME_URL' used in the registration page in the development environment.
+//  * For now, I've made occasional changes to make the "apiVersion" variable
+//  * compatible, but in the future this may also be rewritten
+//  *
+//  * I just kept the libraries because I have no idea of the impact of this
+//  * on the system as a whole, but I already suggest removal in
+//  * later features and rewriting in native apis or .env.{enviroment} file
+//  * like: https://cli.vuejs.org/guide/mode-and-env.html
+//  */
 
-import url from 'url';
-import urljoin from 'url-join';
+// import url from 'url';
+// import urljoin from 'url-join';
 
 function APILocation() {
   const URL = window.location.href;
@@ -35,37 +35,19 @@ function APILocation() {
 
   if (URL.includes('localhost')) {
     return `http://localhost:8011/${apiVersion}`;
-  }
-
-  if (URL.includes('192.168')) {
+  } else if (URL.includes('192.168')) {
     return URL.replace(/:\d+.*/, `:8011/${apiVersion}`);
-  }
-
-  if (URL.includes('test.ufabcnext.com')) {
+  } else if (URL.includes('test.ufabcnext.com')) {
     return `https://ufabc-matricula-test.cdd.naoseiprogramar.com.br/${apiVersion}`;
-  }
-
-  if (URL.includes(`ufabcnext.com`)) {
+  } else {
     return `https://api.ufabcnext.com/${apiVersion}`;
   }
 
-  const parsedURL = url.parse(URL, false, true);
-  return urljoin(parsedURL.protocol, parsedURL.host, `/${apiVersion}`);
-}
-
-function HomeLocation() {
-  const URL = window.location.href;
-
-  if (URL.indexOf('localhost') > 0) {
-    return 'http://localhost:7000';
-  }
-
-  if (URL.indexOf('ufabcnext.com') >= 0) {
-    return 'https://ufabcnext.com';
-  }
+  // I took a closer look at the 'url' library and the nodeJS documentation, and perhaps these lines of code are not needed.
+  // const parsedURL = url.parse(URL, false, true);
+  // return urljoin(parsedURL.protocol, parsedURL.host, `/${apiVersion}`);
 }
 
 export default {
   API_URL: APILocation(),
-  HOME_URL: HomeLocation(),
 };
