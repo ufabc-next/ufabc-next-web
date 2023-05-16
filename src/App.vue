058ffde599a96e68c517a1ab3740c86ea0bc3f6c
Mateus Braga <71946082+mateusbrg@users.noreply.github.com>
2023-05-15 23:30:56 -0300

    feat: fonts and transitions

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/config">Config</router-link>
   </nav>
-  <router-view />
+  <transition name="slide-x-transition" mode="out-in">
+    <router-view />
+  </transition>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<style>
+html {
+  font-family: Roboto, Ubuntu, Helvetica Neue, Helvetica, PingFang SC,
+    Hiragino Sans GB, Microsoft YaHei, SimSun, sans-serif;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+#app {
+  font-family: 'Roboto', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 }
 </style>
