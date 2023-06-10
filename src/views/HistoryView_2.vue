9e7c8d1f33f9bdc1e9cae2df61fcfcb917e0287e
Carlos Alencar <carlos.alencar.99@gmail.com>
2023-06-09 22:03:13 -0300

    fix: history

---
+++ src/views/HistoryView_2.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="history">
+    <h1>History</h1>
+    <button @click="loginDev()">Login Dev</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Auth from '@/services/Auth';
+import User from '@/services/User';
+
+async function loginDev() {
+  try {
+    Auth.token.value = 'DEVTOKEN';
+    localStorage.setItem('token', 'DEVTOKEN');
+    const res = await User.info();
+    if (res.data) Auth.user = res.data;
+  } catch (err) {
+    Auth.user.value = null;
+  }
+}
+</script>
