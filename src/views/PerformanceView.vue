f05171bb2377d2b5eb046f5305dbc07ba71b74d9
Carlos Alencar <carlos.alencar.99@gmail.com>
2023-06-09 22:07:31 -0300

    feat: performance view

---
+++ src/views/PerformanceView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="meu-layout">
+    <PerformanceCard :cr="4.0" title="Seu maior CR até hoje">
+      <v-icon
+        class="mb-1 mdi theme--light mdi-chart-line-variant text-ufabcnext-green"
+      >
+      </v-icon>
+    </PerformanceCard>
+    <PerformanceCard :cr="103" title="Possuem um CR muito próximo ao seu">
+      <v-icon
+        class="mb-1 mdi theme--light mdi-chart-bell-curve text-ufabcnext-blue"
+      >
+      </v-icon>
+    </PerformanceCard>
+    <PerformanceCard :cr="4.0" title="Seu melhor quadrimestre">
+      <v-icon class="mb-1 mdi theme--light mdi-trophy-outline"> </v-icon>
+    </PerformanceCard>
+    <PerformanceCard :cr="4.0" title="Quadrimestre com mais créditos">
+      <v-icon class="mb-1 mdi theme--light mdi-fire"> </v-icon>
+    </PerformanceCard>
+  </div>
+</template>
+
+<script setup lang="ts">
+import PerformanceCard from '@/components/PerformanceCard.vue';
+</script>
+
+<style>
+.meu-layout {
+  display: flex;
+  justify-content: space-between;
+}
+.PerformanceCard {
+  flex: 1;
+  margin: 10px;
+}
+</style>
