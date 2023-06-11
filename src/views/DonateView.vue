c0608ef96804db6a7cebffc37504d542057bd05c
Victor Lourenço <victor.lourenco@tgid.com.br>
2023-06-11 14:50:51 -0300

    refactor: removendo fechamento tag </div> não utilizada e formatando o arquivo no padrão ESLint

--- src/views/DonateView.vue
+++ src/views/DonateView.vue
@@ -28,118 +28,147 @@ export default {
 };
 </script>
 
-
 <template>
   <v-container class="donate-container">
-  <v-dialog v-model="dialog" width="700px">
-    <template v-slot:activator="{ on, attrs }">
-
-      <v-row class="justify-center">
-        <v-col sm12 md12 lg8 class="justify-center">
-          <div class="donation-text">
-            <h2 class="history-title ufabcnext-primary--text">Ajude o UFABC Next</h2>
-            <div class="history-text mt-3 mb-4">
-              Que bom que temos você como nosso usuário 🥰🥳 <br /><br />
-              O Next foi desenvolvido em 2019 <b>de alunos para alunos</b> e desde então é um projeto colaborativo 🤝 , onde você <a href="https://feedback.userreport.com/82aa815c-a1d6-440d-8759-02ccaa78d9fa/#ideas/popular" target="_blank" style="text-decoration: none;">pode opinar, dar sugestões</a> e criar novas funcionalidades, pois é um <a href="https://github.com/ufabc-next" target="_blank" style="text-decoration: none;">projeto open-source.</a>
-              <br /><br />
-              Como nem tudo são flores 🥲, o projeto é mantido pelos seus próprios desenvolvedores, que já gastaram mais de 300h desenvolvendo sem ter nenhum retorno financeiro 🙃, com apenas o objetivo de ganhar conhecimento e colaborar com a comunidade da UFABC.
-              <br /><br />
-              Em 2020, lançamos uma <a href="https://www.kickante.com.br/campanhas/ajude-ufabc-next" target="_blank" style="text-decoration: none;">campanha de crowdfunding</a> que foi sucesso (🎉) e arrecadamos R$ 1.650,00 para manter o projeto no ar até junho de 2022. Como os custos do projeto são mensais e em dólar, precisamos de sua ajuda para continuar com o projeto funcionando e ajudando todos os alunos da UFABC a encontrar um professor ideal, ter uma perspectiva sobre os chutes, além de acompanhar e melhorar sua performance acadêmica 😍. Tudo isso para ajudar os alunos da UFABC a se formar (sabemos que é quase uma missão impossível, mas com o Next fica mais fácil 🚀).
-              <br /><br /> 
-              Abaixo temos os custos detalhados: 
-            </div>
-          </div>
-          </div>
-          <el-table
-            :data="tableData"
-            border
-            style="width: 100%"
-            row-class-name="row-donate-table">
-            <el-table-column
-              prop="name"
-              label="Nome"
-              width="175">
-            </el-table-column>
-            <el-table-column
-              prop="description"
-              label="Descrição">
-            </el-table-column>
-            <el-table-column
-              prop="amount"
-              label="Custo"
-              width="180">
-            </el-table-column>
-            <template v-slot:append>
-              <div class="summary">
-                <div class="summary-text flex">Total de aproximadamente</div>
-                <div class="summary-total">~R$ 1200,00/ano 😬</div>
+    <v-dialog v-model="dialog" width="700px">
+      <template v-slot:activator="{ on, attrs }">
+        <v-row class="justify-center">
+          <v-col sm12 md12 lg8 class="justify-center">
+            <div class="donation-text">
+              <h2 class="history-title ufabcnext-primary--text">
+                Ajude o UFABC Next
+              </h2>
+              <div class="history-text mt-3 mb-4">
+                Que bom que temos você como nosso usuário 🥰🥳 <br /><br />
+                O Next foi desenvolvido em 2019 <b>de alunos para alunos</b> e
+                desde então é um projeto colaborativo 🤝 , onde você
+                <a
+                  href="https://feedback.userreport.com/82aa815c-a1d6-440d-8759-02ccaa78d9fa/#ideas/popular"
+                  target="_blank"
+                  style="text-decoration: none"
+                  >pode opinar, dar sugestões</a
+                >
+                e criar novas funcionalidades, pois é um
+                <a
+                  href="https://github.com/ufabc-next"
+                  target="_blank"
+                  style="text-decoration: none"
+                  >projeto open-source.</a
+                >
+                <br /><br />
+                Como nem tudo são flores 🥲, o projeto é mantido pelos seus
+                próprios desenvolvedores, que já gastaram mais de 300h
+                desenvolvendo sem ter nenhum retorno financeiro 🙃, com apenas o
+                objetivo de ganhar conhecimento e colaborar com a comunidade da
+                UFABC.
+                <br /><br />
+                Em 2020, lançamos uma
+                <a
+                  href="https://www.kickante.com.br/campanhas/ajude-ufabc-next"
+                  target="_blank"
+                  style="text-decoration: none"
+                  >campanha de crowdfunding</a
+                >
+                que foi sucesso (🎉) e arrecadamos R$ 1.650,00 para manter o
+                projeto no ar até junho de 2022. Como os custos do projeto são
+                mensais e em dólar, precisamos de sua ajuda para continuar com o
+                projeto funcionando e ajudando todos os alunos da UFABC a
+                encontrar um professor ideal, ter uma perspectiva sobre os
+                chutes, além de acompanhar e melhorar sua performance acadêmica
+                😍. Tudo isso para ajudar os alunos da UFABC a se formar
+                (sabemos que é quase uma missão impossível, mas com o Next fica
+                mais fácil 🚀). <br /><br />
+                Abaixo temos os custos detalhados:
               </div>
-            </template>
-          </el-table>
-          <v-btn
-            rounded
-            depressed
-            elevation="2"
-            outlined
-            class="dialog-open-btn ufabcnext-primary"
-            v-bind="attrs"
-            v-on="on"
-            >Quero ajudar!</v-btn
-          >
-        </v-col>
-      </v-row>
-    </template>
+            </div>
 
-    <v-card class="dialog-content">
-      <div class="dialog-header">
-        <v-card-title class="dialog-title ufabcnext-darkgrey--text">
-          Informações da conta:
-        </v-card-title>
-        <v-card-actions class="dialog-close-btn">
-          <v-btn icon large>
-            <v-icon color="ufabcnext-darkgrey" @click="dialog = false"
-              >close</v-icon
+            <el-table
+              :data="tableData"
+              border
+              style="width: 100%"
+              row-class-name="row-donate-table"
             >
-          </v-btn>
-        </v-card-actions>
-      </div>
+              <el-table-column prop="name" label="Nome" width="175">
+              </el-table-column>
+              <el-table-column prop="description" label="Descrição">
+              </el-table-column>
+              <el-table-column prop="amount" label="Custo" width="180">
+              </el-table-column>
+              <template v-slot:append>
+                <div class="summary">
+                  <div class="summary-text flex">Total de aproximadamente</div>
+                  <div class="summary-total">~R$ 1200,00/ano 😬</div>
+                </div>
+              </template>
+            </el-table>
+            <v-btn
+              rounded
+              depressed
+              elevation="2"
+              outlined
+              class="dialog-open-btn ufabcnext-primary"
+              v-bind="attrs"
+              v-on="on"
+              >Quero ajudar!</v-btn
+            >
+          </v-col>
+        </v-row>
+      </template>
 
-      <v-card-text
-        class="dialog-body"
-        :style="{'display': $vuetify.breakpoint.smAndDown ? 'block' : 'flex',
-          'padding': $vuetify.breakpoint.smAndDown ? '0' : '0 40px'
+      <v-card class="dialog-content">
+        <div class="dialog-header">
+          <v-card-title class="dialog-title ufabcnext-darkgrey--text">
+            Informações da conta:
+          </v-card-title>
+          <v-card-actions class="dialog-close-btn">
+            <v-btn icon large>
+              <v-icon color="ufabcnext-darkgrey" @click="dialog = false"
+                >close</v-icon
+              >
+            </v-btn>
+          </v-card-actions>
+        </div>
+
+        <v-card-text
+          class="dialog-body"
+          :style="{
+            display: $vuetify.breakpoint.smAndDown ? 'block' : 'flex',
+            padding: $vuetify.breakpoint.smAndDown ? '0' : '0 40px',
           }"
-      >
-        <div class="dialog-body-qrcode">
-          <img
-            src="@/assets/pix-fe.png"
-            :style="{'width': $vuetify.breakpoint.smAndDown ? '200px' : '185px'}"
-          />
-        </div>
-        <div
-          class="dialog-body-account"
-          :style="{'marginLeft': $vuetify.breakpoint.smAndDown ? '10px' : '30px'}"
         >
-          <p class="dialog-body-account-item">Nome: Felipe Oliveira Silva</p>
-          <p class="dialog-body-account-item">
-            Chave Pix (aleatória): <br /><b>4e031758-52fb-4c9c-876c-894dcaa6afe9</b>
-          </p>
-          <a href="https://nubank.com.br/pagar/1b0zby/HVkAkLCsHr"
-            ><p>Clique e contribua!</p></a
+          <div class="dialog-body-qrcode">
+            <img
+              src="@/assets/pix-fe.png"
+              :style="{
+                width: $vuetify.breakpoint.smAndDown ? '200px' : '185px',
+              }"
+            />
+          </div>
+          <div
+            class="dialog-body-account"
+            :style="{
+              marginLeft: $vuetify.breakpoint.smAndDown ? '10px' : '30px',
+            }"
           >
-        </div>
-      </v-card-text>
+            <p class="dialog-body-account-item">Nome: Felipe Oliveira Silva</p>
+            <p class="dialog-body-account-item">
+              Chave Pix (aleatória): <br /><b
+                >4e031758-52fb-4c9c-876c-894dcaa6afe9</b
+              >
+            </p>
+            <a href="https://nubank.com.br/pagar/1b0zby/HVkAkLCsHr"
+              ><p>Clique e contribua!</p></a
+            >
+          </div>
+        </v-card-text>
 
-      <v-card-text class="dialog-footer">
-        <div>
-          O NEXT SÓ EXISTE POR PESSOAS COMO VOCÊ 😍
-        </div>
-      </v-card-text>
-    </v-card>
-  </v-dialog>
-</v-container>
+        <v-card-text class="dialog-footer">
+          <div>O NEXT SÓ EXISTE POR PESSOAS COMO VOCÊ 😍</div>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </v-container>
 </template>
-
 
 <style>
 .donate-container {
