5a5ba47669e60b492d8e0f8512b611d4065e29d9
Mateus Braga <71946082+mateusbrg@users.noreply.github.com>
2023-08-01 00:07:36 -0300

    feat: type performance requests

---
+++ src/services/Performance.ts
@@ -0,0 +1,28 @@
+import api from '@/utils/api';
+
+type QuadInformation = {
+  accumulated_credits: number;
+  ca_acumulado: number;
+  ca_quad: number;
+  cp_acumulado: number;
+  cr_acumulado: number;
+  cr_quad: number;
+  percentage_approved: number;
+  period_credits: number;
+  quad: number;
+  season: string;
+  year: number;
+};
+
+type CrDistributionData = {
+  point: string;
+  total: number;
+  _id: string;
+};
+
+const performance = {
+  getCrHistory: () => api.get<QuadInformation[]>('users/me/grades'),
+  getCrDistribution: () => api.get<CrDistributionData[]>('stats/grades'),
+};
+
+export default performance;
