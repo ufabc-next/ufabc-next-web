f72b58c4c725e09d7cb9c3b90d44e2e3278db5da
Joabesv <joabevarjao123@gmail.com>
2023-08-04 02:10:10 -0300

    create app types

---
+++ apps/ufabc-next-backend/packages/types/index.d.ts
@@ -0,0 +1,2 @@
+import type { User, UserMethods, UserModel } from './model/user';
+export { User, UserMethods, UserModel };
---
+++ apps/ufabc-next-backend/packages/types/model/user.d.ts
@@ -0,0 +1,33 @@
+import type { Model, ObjectId, Schema } from 'mongoose';
+
+export type User = {
+  email: string;
+  oauth: {
+    email: string;
+    provider: string;
+    providerId: string;
+    picture?: string | undefined;
+  };
+  confirmed: boolean;
+  ra: number;
+  active: boolean;
+  permissions: string[];
+  createdAt: string;
+  devices: {
+    deviceId: string;
+    token: string;
+    phone: string;
+  }[];
+};
+
+type UserMethods = {
+  addDevice(device: User['devices']): void
+  removeDevice(deviceId: ObjectId): ObjectId[]
+  sendConfirmation(): Promise<void>
+  generateJWT(): string;
+}
+
+export type UserModel = Model<User, unknown, UserMethods>;
+// Just in case
+type UserSchema = Schema<User, UserModel, UserMethods>;
+
