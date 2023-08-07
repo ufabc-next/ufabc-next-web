a9f74fcfd8537a1bc18cc8889520cd91a7c5c531
Joabesv <joabevarjao123@gmail.com>
2023-08-07 01:16:31 -0300

    core(plugins): create jwt

---
+++ apps/ufabc-next-backend/apps/core/src/plugins/jwt.ts
@@ -0,0 +1,14 @@
+import type { FastifyInstance } from 'fastify';
+import type { Config } from '@config';
+import { fastifyJwt } from '@fastify/jwt';
+
+export default async function jwtAuth(app: FastifyInstance, opts: Config) {
+  try {
+    await app.register(fastifyJwt, {
+      secret: opts.JWT_SECRET,
+    });
+    app.log.info('[PLUGIN] JsonWebToken');
+  } catch (error) {
+    app.log.error({ error }, '[PLUGIN] error in JsonWebToken');
+  }
+}
