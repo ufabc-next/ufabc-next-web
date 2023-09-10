a46624b64e2eb181bcd7c07605ca6b78b59e1056
Joabe Varjao <joabe.varjao@layers.education>
2023-09-10 18:30:05 -0300

    better env setup

--- apps/ufabc-next-backend/apps/core/src/config/config.ts
+++ apps/ufabc-next-backend/apps/core/src/config/config.ts
@@ -10,42 +10,56 @@ if (process.env.NODE_ENV === 'test') {
 const envSchema = z.object({
   // Local machine
   NODE_ENV: z.enum(['dev', 'test', 'prod']).default('dev'),
-  PORT: z.coerce.number().default(3000),
+  PORT: z.coerce.number().default(5000),
   HOST: z.string().min(4).default('localhost'),
-  WEB_URL: z.string().url(),
-  JWT_SECRET: z.string().min(32),
-  // AWS credentials
-  AWS_REGION: z.string(),
-  AWS_ACCESS_KEY_ID: z.string(),
-  AWS_SECRET_ACCESS_KEY: z.string(),
-  // EMAIL
-  EMAIL_CONFIRMATION_TEMPLATE: z.string(),
-  MAILER_ID: z.string(),
-  // OAUTH2
-  OAUTH_FACEBOOK_CLIENT_ID: z.string(),
-  OAUTH_FACEBOOK_SECRET: z.string().min(16),
-  OAUTH_GOOGLE_CLIENT_ID: z.string(),
-  OAUTH_GOOGLE_SECRET: z.string().min(16),
+
+  // default to legacy front-end url
+  WEB_URL: z.string().url().default('http://localhost:7500/app/#'),
+  // Random string
+  JWT_SECRET: z.string().min(32).default('as4MDFhi0OxGtfQCO6wFC68H5qdZ4zpc'),
+
+  // AWS credentials - change with the ones from your account
+  AWS_REGION: z.string().default('us-east-1'),
+  AWS_ACCESS_KEY_ID: z.string().default('AHUROUY6XAGX89PUFYNB'),
+  AWS_SECRET_ACCESS_KEY: z
+    .string()
+    .default('DNUZ3E8IUOXULCQERTCXNC6BMZCV19UBNZOPWWS3'),
+
+  // Ses E-mail
+  EMAIL_CONFIRMATION_TEMPLATE: z.string().default('Confirmation'),
+  EMAIL_RECOVERY_TEMPLATE: z.string().default('Recovery'),
+  EMAIL: z.string().email().default('contato@ufabcnext.com'),
+
+  // OAUTH2 - Change with the one you generate
+  OAUTH_FACEBOOK_CLIENT_ID: z.coerce.number().default(4859485948),
+  OAUTH_FACEBOOK_SECRET: z.string().min(16).default('0sraUa6lA7oSdewe'),
+  OAUTH_GOOGLE_CLIENT_ID: z
+    .string()
+    .default('12121212-random-generated-token.apps.googleusercontent.com'),
+  OAUTH_GOOGLE_SECRET: z
+    .string()
+    .min(16)
+    .default('GOCSPX-random-generated-token'),
 
   // MONGODB
-  MONGODB_USER: z.string(),
+  MONGODB_NAME: z.string().default('ufabc-next-backend'),
+  MONGODB_USER: z.string().default('localUser'),
+  MONGODB_PASSWORD: z.string().min(6).default('localSecret'),
   MONGODB_PORT: z.coerce.number().default(27017),
-  MONGODB_PASSWORD: z.string().min(6),
-  MONGODB_NAME: z.string(),
 
   // Redis
-  REDIS_NAME: z.string(),
+  REDIS_NAME: z.string().default('ufabc-next-redis'),
   REDIS_USER: z.string().default('default'),
-  REDIS_PASSWORD: z.string().min(8),
+  REDIS_PASSWORD: z.string().min(8).default('localRedis'),
   REDIS_PORT: z.coerce.number().default(6379),
 
   // Docker URL
-  MONGODB_CONNECTION_URL: z.string(),
-  REDIS_CONNECTION_URL: z.string(),
-
-  // Aws
-  secretAccessKey: z.string().optional(),
-  accessKeyId: z.string().optional(),
+  MONGODB_CONNECTION_URL: z
+    .string()
+    .default(`mongodb://localUser:localSecret@127.0.0.1:27017`),
+  REDIS_CONNECTION_URL: z
+    .string()
+    .default('redis://default:localRedis@127.0.0.1:6379'),
 });
 
 const _env = envSchema.safeParse(process.env);
