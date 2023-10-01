1da5565842db182c31ffbc17b752b294ca2fe1a1
Joabesv <joabevarjao123@gmail.com>
2023-10-01 01:04:13 -0300

    update build setup

--- apps/ufabc-next-backend/apps/core/esbuild.ts
+++ apps/ufabc-next-backend/apps/core/esbuild.ts
@@ -1,19 +1,21 @@
 import { build } from 'esbuild';
 import esbuildPluginPino from 'esbuild-plugin-pino';
 import glob from 'tiny-glob';
+const allFiles = await glob('src/**/*.ts');
+const handlersFiles = await glob('src/**/handlers/*.ts');
+const entryPoints = allFiles.filter((files) => !handlersFiles.includes(files));
 
-void (async () => {
-  const entryPoints = await glob('src/**/*.ts');
-  await build({
-    entryPoints,
-    logLevel: 'info',
-    outdir: 'dist',
-    target: 'esnext',
-    bundle: true,
-    sourcemap: true,
-    platform: 'node',
-    format: 'esm',
-    packages: 'external',
-    plugins: [esbuildPluginPino({ transports: ['pino-pretty'] })],
-  });
-})();
+await build({
+  entryPoints: allFiles,
+  logLevel: 'info',
+  outdir: 'dist',
+  target: 'esnext',
+  bundle: true,
+  sourcemap: true,
+  platform: 'node',
+  format: 'esm',
+  // packages: 'external',
+  packages: 'external',
+  // @ts-expect-error Library problem
+  plugins: [esbuildPluginPino({ transports: ['pino-pretty'] })],
+});
