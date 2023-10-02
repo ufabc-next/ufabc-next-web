7adfa7f2d94c401c3cdc2c3b2b5db187d11ea195
RenanLorijola <renanzago@gmail.com>
2023-10-02 01:46:51 -0300

    chore: jest vuetify config

--- apps/container/jest.setup.js
+++ apps/container/jest.setup.js
@@ -1,6 +1,22 @@
 import '@testing-library/jest-dom';
 import { server } from '@/mocks/server';
 
+class ResizeObserverStub {
+  observe() {
+    // do nothing
+  }
+  unobserve() {
+    // do nothing
+  }
+  disconnect() {
+    // do nothing
+  }
+}
+
+window.ResizeObserver = window.ResizeObserver || ResizeObserverStub;
+
+global.CSS = { supports: () => false };
+
 beforeAll(() => server.listen());
 
 afterEach(() => server.resetHandlers());
--- apps/container/src/vuetify.ts
+++ apps/container/src/vuetify.ts
@@ -1,29 +1,29 @@
-import { ThemeDefinition, createVuetify } from 'vuetify';
+import { createVuetify } from 'vuetify';
 
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 
-const myCustomLightTheme: ThemeDefinition = {
-  dark: false,
-  colors: {
-    navigation: '#215096',
-    primary: '#2e7eed',
-    secondary: '#f3f6f7',
-    'ufabcnext-green': '#56cdb7',
-    'next-gray': '#404040',
-    'next-light-gray': '#848687',
-    error: '#f45576',
-    background: '#fffff````f',
-  },
-};
+// const myCustomLightTheme: ThemeDefinition = {
+//   dark: false,
+//   colors: {
+//     navigation: '#215096',
+//     primary: '#2e7eed',
+//     secondary: '#f3f6f7',
+//     'ufabcnext-green': '#56cdb7',
+//     'next-gray': '#404040',
+//     'next-light-gray': '#848687',
+//     error: '#f45576',
+//     background: '#fffff````f',
+//   },
+// };
 
 export const vuetify = createVuetify({
   components,
   directives,
-  theme: {
-    defaultTheme: 'myCustomLightTheme',
-    themes: {
-      myCustomLightTheme,
-    },
-  },
+  // theme: {
+  //   defaultTheme: 'myCustomLightTheme',
+  //   themes: {
+  //     myCustomLightTheme,
+  //   },
+  // },
 });
