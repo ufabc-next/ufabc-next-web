85fba2abac76c0596a77d9d622ca0810b8987708
RenanLorijola <renanzago@gmail.com>
2023-10-07 18:16:12 -0300

    fix: user mock typo

--- apps/container/src/mocks/users.ts
+++ apps/container/src/mocks/users.ts
@@ -1,13 +1,16 @@
-export const user = {
+import { User } from 'services';
+
+export const user: User = {
   _id: '123',
   confirmed: true,
-  createAt: '2021-01-01',
+  createdAt: '2021-01-01',
   devices: [],
   oauth: {
     email: 'email',
     emailFacebook: 'emailFacebook',
     facebook: 'facebook',
     picture: 'picture',
+    google: 'google',
   },
   ra: 123,
   email: 'email@email.com',
