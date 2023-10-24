79c2e3622fb32b9f8616974c1ee07a7d4efe381f
Joabesv <joabevarjao123@gmail.com>
2023-10-23 22:39:07 -0300

    chore: remove dotenv from core

--- apps/ufabc-next-backend/apps/core/src/config/config.ts
+++ apps/ufabc-next-backend/apps/core/src/config/config.ts
@@ -1,11 +1,4 @@
-import { config as dotEnvConfig } from 'dotenv';
 import { z } from 'zod';
-
-if (process.env.NODE_ENV === 'test') {
-  dotEnvConfig({ path: '.env.test' });
-} else {
-  dotEnvConfig();
-}
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'prod']).default('dev'),
