19ea72bfc2b56f9241679d79b3473a2d04403e15
RenanLorijola <renanzago@gmail.com>
2023-10-29 03:55:58 -0300

    chore: update dependencies

--- apps/container/src/mocks/handlers.ts
+++ apps/container/src/mocks/handlers.ts
@@ -1,14 +1,10 @@
-import { rest } from 'msw';
+import { http, HttpResponse } from 'msw';
 import { user } from './users';
 import { enrollments } from './enrollments';
 
 const baseUrl = 'https://api.ufabcnext.com/v1';
 
 export const handlers = [
-  rest.get(`${baseUrl}/users/info`, (req, res, ctx) =>
-    res(ctx.status(200), ctx.json(user)),
-  ),
-  rest.get(`${baseUrl}/enrollments`, (req, res, ctx) =>
-    res(ctx.status(200), ctx.json(enrollments)),
-  ),
+  http.get(`${baseUrl}/users/info`, () => HttpResponse.json(user)),
+  http.get(`${baseUrl}/enrollments`, () => HttpResponse.json(enrollments)),
 ];
