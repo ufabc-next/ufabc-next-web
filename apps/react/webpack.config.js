d3ca92718aa5d1bb46040f7c73bdbe2b8ce79a45
RenanLorijola <renanzago@gmail.com>
2023-11-11 14:31:08 -0300

    fix: add correct dependecies to react plugins

--- apps/react/webpack.config.js
+++ apps/react/webpack.config.js
@@ -45,7 +45,18 @@ module.exports = {
       exposes: {
         './Calengrade': './src/components/Calengrade',
       },
-      shared: dependencies,
+      shared: {
+        react: {
+          singleton: true,
+          eager: true,
+          requiredVersion: dependencies.react,
+        },
+        'react-dom': {
+          singleton: true,
+          eager: true,
+          requiredVersion: dependencies['react-dom'],
+        },
+      },
     }),
   ],
   devServer: {
