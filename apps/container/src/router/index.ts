f977983af38e80034f31aa7b128543034e8bc6f6
FusiDaniel <fpires.daniel@gmail.com>
2023-11-13 17:30:47 -0300

    fix: redirect confirmed user

--- apps/container/src/router/index.ts
+++ apps/container/src/router/index.ts
@@ -149,16 +149,24 @@ router.beforeEach(async (to, _from, next) => {
   }
 
   if (to.matched.some((record) => record.meta.auth === true)) {
+    // if user doens't have a token, redirect
     if (!authStore.getState().token) {
       if (process.env.VUE_APP_MF_ENV !== 'local') {
         return (window.location.pathname = '/');
       }
       return next();
     }
-    if (
-      to.matched.some((record) => record.meta.confirmed === true) &&
-      !authStore.getState().user?.confirmed
-    ) {
+
+    // if user needs to be confirmed and it is, don't redirect
+    if (to.matched.some((record) => record.meta.confirmed === true)) {
+      if (authStore.getState().user?.confirmed) {
+        return next();
+      }
+      return next('/review');
+    }
+
+    // if user doesn't need to be confirmed and it is, redirect
+    if (authStore.getState().user?.confirmed && !to.matched.some((record) => record.path === '/reviews') ) {
       return next('/review');
     }
     return next();
