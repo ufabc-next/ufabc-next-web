8ea9d79efcb764b9bcd99f8501f94fee5427a76e
Joabe Silva <73668618+Joabesv@users.noreply.github.com>
2023-11-17 01:43:09 -0300

    Update FacebookAuthView.vue

--- apps/ufabc-next-web/web/src/pages/FacebookLogin/FacebookAuthView.vue
+++ apps/ufabc-next-web/web/src/pages/FacebookLogin/FacebookAuthView.vue
@@ -55,7 +55,7 @@ export default {
       await User.facebookAuth(payload).then(({data}) => {
         console.log('request data', data)
         Auth.setToken(data.token)
-        this.$router.push('/reviews')
+        // this.$router.push('/reviews')
         // const teste = `https://api.ufabcnext.com/connect/google?userId=${res.data.userId}`
         // await axios.get(teste).catch(err => console.log(err))
 
