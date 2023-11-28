6930cd9976be70ff2e505a65aca418a587c2059f
Joabesv <joabevarjao123@gmail.com>
2023-11-28 20:57:05 -0300

    chore: init new home package

---
+++ apps/home/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import { fileURLToPath } from 'node:url';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
