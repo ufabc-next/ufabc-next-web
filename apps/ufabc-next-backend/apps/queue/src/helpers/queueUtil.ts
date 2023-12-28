fd4003dd916c6090c5815af5bacf79fb53398112
Joabesv <joabevarjao123@gmail.com>
2023-12-28 16:44:38 -0300

    rollback queue types

--- apps/ufabc-next-backend/apps/queue/src/helpers/queueUtil.ts
+++ apps/ufabc-next-backend/apps/queue/src/helpers/queueUtil.ts
@@ -1,5 +1,4 @@
 import {
-  type Job,
   type Processor,
   Queue,
   type RedisOptions,
@@ -20,8 +19,7 @@ const connection = {
  * Creates a queue with the given name
  * creates a connection in every instance(aka if you have 3 instances, it will create 3 connections)
  * */
-export const createQueue = <TQueueData>(name: string) =>
-  new Queue<Job<TQueueData>>(name, { connection });
+export const createQueue = (name: string) => new Queue(name, { connection });
 
 /**
  * Creates a worker attached to a given queue
