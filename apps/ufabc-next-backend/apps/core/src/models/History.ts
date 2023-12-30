2135c6b335742a9f50b25d70f22cb6deeeeab142
Joabesv <joabevarjao123@gmail.com>
2023-12-30 17:31:38 -0300

    refac(models): adjust the history model

--- apps/ufabc-next-backend/apps/core/src/models/History.ts
+++ apps/ufabc-next-backend/apps/core/src/models/History.ts
@@ -1,21 +1,24 @@
-import {
-  type InferSchemaType,
-  Schema,
-  type UpdateQuery,
-  model,
-} from 'mongoose';
-import { addUserEnrollmentsToQueue } from '@/queue/jobs/userEnrollmentsUpdate.js';
-import { EnrollmentModel } from './Enrollment.js';
-import { GraduationModel } from './Graduation.js';
-import { GraduationHistoryModel } from './GraduationHistory.js';
-import { SubjectModel } from './Subject.js';
+import { type InferSchemaType, Schema, model } from 'mongoose';
+// let userEnrollmentsJob: any;
 
-let userEnrollmentsJob: any;
+const historyDisciplinaSchema = new Schema(
+  {
+    periodo: String,
+    codigo: String,
+    disciplina: String,
+    ano: Number,
+    situacao: String,
+    creditos: Number,
+    categoria: String,
+    conceito: String,
+  },
+  { _id: false },
+);
 
 const historySchema = new Schema(
   {
     ra: Number,
-    disciplinas: Object,
+    disciplinas: [historyDisciplinaSchema],
     coefficients: Object,
     curso: String,
     grade: String,
@@ -23,14 +26,14 @@ const historySchema = new Schema(
   {
     methods: {
       async updateEnrollments() {
-        userEnrollmentsJob = {
-          doc: this.toObject<History>({ virtuals: true }),
-          enrollmentModel: EnrollmentModel,
-          graduationModel: GraduationModel,
-          graduationHistoryModel: GraduationHistoryModel,
-          subjectModel: SubjectModel,
-        };
-        await addUserEnrollmentsToQueue(userEnrollmentsJob);
+        // userEnrollmentsJob = {
+        //   doc: this.toObject<History>({ virtuals: true }),
+        //   enrollmentModel: EnrollmentModel,
+        //   graduationModel: GraduationModel,
+        //   graduationHistoryModel: GraduationHistoryModel,
+        //   subjectModel: SubjectModel,
+        // };
+        // await addUserEnrollmentsToQueue(userEnrollmentsJob);
       },
     },
     timestamps: true,
@@ -43,24 +46,23 @@ historySchema.pre('findOneAndUpdate', async function () {
   //why does the legacy code pass things that aren't in the schema?
   //also, the updateUserEnrollments cron job in the legacy code doesn't use the mandatory_credits_number, limited_credits_number, free_credits_number, credits_total properties
   // calls cron job here
-  const update: UpdateQuery<History> | null = this.getUpdate();
-  if (!update) {
-    // if theres nothing to update, do nothing
-    return;
-  }
-  const updateJob = {
-    ra: update.ra,
-    disciplinas: update.disciplinas,
-    curso: update.curso,
-    grade: update.grade,
-    mandatory_credits_number: update.mandatory_credits_number,
-    limited_credits_number: update.limited_credits_number,
-    free_credits_number: update.free_credits_number,
-    credits_total: update.credits_total,
-  };
-  userEnrollmentsJob.doc = updateJob;
-
-  await addUserEnrollmentsToQueue(userEnrollmentsJob);
+  // const update: UpdateQuery<History> | null = this.getUpdate();
+  // if (!update) {
+  //   // if theres nothing to update, do nothing
+  //   return;
+  // }
+  // const updateJob = {
+  //   ra: update.ra,
+  //   disciplinas: update.disciplinas,
+  //   curso: update.curso,
+  //   grade: update.grade,
+  //   mandatory_credits_number: update.mandatory_credits_number,
+  //   limited_credits_number: update.limited_credits_number,
+  //   free_credits_number: update.free_credits_number,
+  //   credits_total: update.credits_total,
+  // };
+  // userEnrollmentsJob.doc = updateJob;
+  // await addUserEnrollmentsToQueue(userEnrollmentsJob);
   // app.agenda.now('updateUserEnrollments', [
   //   ra,
   //   disciplinas,
@@ -74,8 +76,8 @@ historySchema.pre('findOneAndUpdate', async function () {
 });
 
 historySchema.post('save', async function () {
-  userEnrollmentsJob.doc = this.toObject({ virtuals: true });
-  await addUserEnrollmentsToQueue(userEnrollmentsJob);
+  // userEnrollmentsJob.doc = this.toObject({ virtuals: true });
+  // await addUserEnrollmentsToQueue(userEnrollmentsJob);
 });
 
 export type History = InferSchemaType<typeof historySchema>;
