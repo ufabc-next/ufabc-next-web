f8408bf3f80f9cccb53aa1299df643fd142a01ab
Joabesv <joabevarjao123@gmail.com>
2024-01-05 11:20:37 -0300

    feat(teachers): parse ufabc teachers file

--- apps/ufabc-next-backend/apps/core/src/modules/disciplinas/route.ts
+++ apps/ufabc-next-backend/apps/core/src/modules/disciplinas/route.ts
@@ -1,7 +1,17 @@
+import { isAdminValidator } from '../private/isAdmin.js';
 import { listDisciplinas } from './handlers/listDisciplinas.js';
+import {
+  type ParseTeachersRequest,
+  parseTeachersHandler,
+} from './handlers/parseTeachers.js';
 import type { FastifyInstance } from 'fastify';
 
 // eslint-disable-next-line require-await
 export async function disciplinasRoute(app: FastifyInstance) {
   app.get('/', listDisciplinas);
+  app.put<ParseTeachersRequest>(
+    '/teachers',
+    { preValidation: [isAdminValidator] },
+    parseTeachersHandler,
+  );
 }
