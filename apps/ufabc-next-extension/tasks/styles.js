6e6ea8ce37cfa4cd223cdc6dfccc4027b8cddaaf
Joabesv <joabevarjao123@gmail.com>
2024-02-04 23:59:07 -0300

    fix: remove unused and broken gulp task steps

--- apps/ufabc-next-extension/tasks/styles.js
+++ apps/ufabc-next-extension/tasks/styles.js
@@ -1,57 +1,64 @@
-import gulp from 'gulp'
-import gulpif from 'gulp-if'
-import gutil from 'gulp-util'
-import sourcemaps from 'gulp-sourcemaps'
-import less from 'gulp-less'
+import gulp from "gulp";
+import gulpif from "gulp-if";
+import gutil from "gulp-util";
+import sourcemaps from "gulp-sourcemaps";
+import less from "gulp-less";
 
-import dartSass from 'sass';
-import gulpSass from 'gulp-sass';
+import dartSass from "sass";
+import gulpSass from "gulp-sass";
 const sass = gulpSass(dartSass);
 
-import cleanCSS from 'gulp-clean-css'
-import livereload from 'gulp-livereload'
-import cssimport from 'gulp-cssimport'
-import args from './lib/args'
-import gulpWebpack from 'webpack-stream'
+import cleanCSS from "gulp-clean-css";
+import livereload from "gulp-livereload";
+import cssimport from "gulp-cssimport";
+import args from "./lib/args";
+import gulpWebpack from "webpack-stream";
 
-gulp.task('styles:css', function () {
-  return gulp.src('app/styles/*.css')
+gulp.task("styles:css", function () {
+  return gulp
+    .src("app/styles/*.css")
     .pipe(cssimport())
     .pipe(gulpif(args.sourcemaps, sourcemaps.init()))
     .pipe(gulpif(args.production, cleanCSS()))
     .pipe(gulpif(args.sourcemaps, sourcemaps.write()))
     .pipe(gulp.dest(`dist/${args.vendor}/styles`))
-    .pipe(gulpif(args.watch, livereload()))
-})
+    .pipe(gulpif(args.watch, livereload()));
+});
 
-gulp.task('styles:less', function () {
-  return gulp.src('app/styles/*.less')
+gulp.task("styles:less", function () {
+  return gulp
+    .src("app/styles/*.less")
     .pipe(gulpif(args.sourcemaps, sourcemaps.init()))
-    .pipe(less({ paths: ['./app'] }).on('error', function (error) {
-      gutil.log(gutil.colors.red('Error (' + error.plugin + '): ' + error.message))
-      this.emit('end')
-    }))
+    .pipe(
+      less({ paths: ["./app"] }).on("error", function (error) {
+        gutil.log(
+          gutil.colors.red("Error (" + error.plugin + "): " + error.message),
+        );
+        this.emit("end");
+      }),
+    )
     .pipe(gulpif(args.production, cleanCSS()))
     .pipe(gulpif(args.sourcemaps, sourcemaps.write()))
     .pipe(gulp.dest(`dist/${args.vendor}/styles`))
-    .pipe(gulpif(args.watch, livereload()))
-})
+    .pipe(gulpif(args.watch, livereload()));
+});
 
-gulp.task('styles:sass', function () {
-  return gulp.src('app/styles/*.scss')
+gulp.task("styles:sass", function () {
+  return gulp
+    .src("app/styles/*.scss")
     .pipe(gulpif(args.sourcemaps, sourcemaps.init()))
-    .pipe(sass({ includePaths: ['./app'] }).on('error', function (error) {
-      gutil.log(gutil.colors.red('Error (' + error.plugin + '): ' + error.message))
-      this.emit('end')
-    }))
+    .pipe(
+      sass({ includePaths: ["./app"] }).on("error", function (error) {
+        gutil.log(
+          gutil.colors.red("Error (" + error.plugin + "): " + error.message),
+        );
+        this.emit("end");
+      }),
+    )
     .pipe(gulpif(args.production, cleanCSS()))
     .pipe(gulpif(args.sourcemaps, sourcemaps.write()))
     .pipe(gulp.dest(`dist/${args.vendor}/styles`))
-    .pipe(gulpif(args.watch, livereload()))
-})
+    .pipe(gulpif(args.watch, livereload()));
+});
 
-gulp.task('styles', gulp.series(
-  'styles:css',
-  'styles:less',
-  'styles:sass'
-))
+gulp.task("styles", gulp.series("styles:css", "styles:sass"));
