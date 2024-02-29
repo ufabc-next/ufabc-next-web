0fd1fc5b2579f21722a08d8aeedab95b341b7cea
Joabesv <joabevarjao123@gmail.com>
2024-02-29 15:32:28 -0300

    chore: route to know if somebody is kicked

--- apps/ufabc-next-backend/apps/core/src/modules/disciplinas/route.ts
+++ apps/ufabc-next-backend/apps/core/src/modules/disciplinas/route.ts
@@ -1,6 +1,7 @@
 import { authenticate } from '@/hooks/authenticate.js';
 import { isAdminValidator } from '../private/isAdmin.js';
 import { listDisciplinas } from './handlers/listDisciplinas.js';
+import { getDisciplinasKicks } from './handlers/getDisciplinasKicks.js';
 import {
   type ParseTeachersRequest,
   parseTeachersHandler,
@@ -10,6 +11,7 @@ import type { FastifyInstance } from 'fastify';
 // eslint-disable-next-line require-await
 export async function disciplinasRoute(app: FastifyInstance) {
   app.get('/', listDisciplinas);
+  app.get('/:disciplinaId/kicks', getDisciplinasKicks);
   app.put<ParseTeachersRequest>(
     '/teachers',
     { preValidation: [isAdminValidator], onRequest: [authenticate] },
