1a36073c9dd60493c446702595a6769174811ffa
Joabesv <joabevarjao123@gmail.com>
2024-03-01 10:26:07 -0300

    chore: stricter history schema and better types

--- apps/ufabc-next-backend/apps/core/src/models/GraduationHistory.ts
+++ apps/ufabc-next-backend/apps/core/src/models/GraduationHistory.ts
@@ -1,8 +1,12 @@
-import { type InferSchemaType, Schema, model } from 'mongoose';
+import {
+  type InferSchemaType,
+  type Model,
+  Schema,
+  type Types,
+  model,
+} from 'mongoose';
 import { mongooseLeanVirtuals } from 'mongoose-lean-virtuals';
 import type { CoefficientsMap } from './History.js';
-
-type GraduationHistoryCoefficients = Record<number, CoefficientsMap>;
 
 const CONCEITOS = ['A', 'B', 'C', 'D', 'O', 'F', '-'] as const;
 const POSSIBLE_SITUATIONS = [
@@ -13,36 +17,63 @@ const POSSIBLE_SITUATIONS = [
   'Apr.S.Nota',
   'Aproveitamento',
 ] as const;
+const CATEGORIES = ['-', 'Opção Limitada', 'Obrigatória'] as const;
+const PERIODO = ['1', '2', '3'] as const;
+
+type GraduationHistoryCoefficients = Record<number, CoefficientsMap>;
+type GraduationHistory = {
+  ra: number;
+  disciplinas: InferSchemaType<typeof GraduationHistoryDisciplinasSchema>[];
+  curso: string;
+  grade: string;
+  graduation: Types.ObjectId;
+  coefficients: GraduationHistoryCoefficients[];
+};
+
+type GraduationHistoryModel = Model<GraduationHistory, {}>;
 
 const GraduationHistoryDisciplinasSchema = new Schema(
   {
-    periodo: String,
-    codigo: String,
-    disciplina: String,
-    ano: Number,
+    periodo: { type: String, required: true, enum: PERIODO },
+    codigo: { type: String, required: true },
+    disciplina: { type: String, required: true },
+    ano: { type: Number, required: true },
     situacao: {
       type: String,
       enum: POSSIBLE_SITUATIONS,
+      required: true,
     },
-    creditos: Number,
-    categoria: String,
+    creditos: { type: Number, required: true },
+    categoria: {
+      type: String,
+      required: true,
+      enum: CATEGORIES,
+    },
     conceito: {
       type: String,
       enum: CONCEITOS,
+      required: true,
     },
-    identifier: String,
+    identifier: { type: String, required: false },
   },
   { _id: false },
 );
-const graduationHistorySchema = new Schema(
+
+const graduationHistorySchema = new Schema<
+  GraduationHistory,
+  GraduationHistoryModel
+>(
   {
-    ra: Number,
-    coefficients: Object as unknown as GraduationHistoryCoefficients,
+    ra: {
+      type: Number,
+      required: true,
+    },
+    coefficients: Object,
 
     disciplinas: [GraduationHistoryDisciplinasSchema],
 
     curso: { type: String, required: true },
-    grade: String,
+    grade: { type: String, required: true },
     graduation: {
       type: Schema.Types.ObjectId,
       ref: 'graduation',
@@ -52,8 +83,8 @@ const graduationHistorySchema = new Schema(
 );
 
 graduationHistorySchema.plugin(mongooseLeanVirtuals);
-export type GraduationHistory = InferSchemaType<typeof graduationHistorySchema>;
-export const GraduationHistoryModel = model(
-  'historiesgraduations',
-  graduationHistorySchema,
-);
+export type { GraduationHistory };
+export const GraduationHistoryModel = model<
+  GraduationHistory,
+  GraduationHistoryModel
+>('historiesgraduations', graduationHistorySchema);
