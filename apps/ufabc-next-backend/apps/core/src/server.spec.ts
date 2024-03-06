439b6167c86566f5d80da60fc285133df290ec7a
Joabesv <joabevarjao123@gmail.com>
2024-03-05 23:13:54 -0300

    test: remove .only

--- apps/ufabc-next-backend/apps/core/src/server.spec.ts
+++ apps/ufabc-next-backend/apps/core/src/server.spec.ts
@@ -2,7 +2,7 @@ import assert from 'node:assert/strict';
 import { buildApp } from './app.js';
 import type { FastifyInstance } from 'fastify';
 
-describe.only('Server', () => {
+describe('Server', () => {
   let app: FastifyInstance;
   beforeAll(async () => {
     app = await buildApp();
