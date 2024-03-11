08851beb7c350773793f9dbb57e1c6559e4626c2
Joabesv <joabevarjao123@gmail.com>
2024-03-11 20:34:55 -0300

    chore: deprecate old history

--- apps/ufabc-next-backend/apps/core/src/modules/histories/route.ts
+++
@@ -1,7 +0,0 @@
-import { createHistory } from './handlers/createHistory.js';
-import type { FastifyInstance } from 'fastify';
-
-// eslint-disable-next-line require-await
-export async function historiesRoute(app: FastifyInstance) {
-  app.post('/', createHistory);
-}
