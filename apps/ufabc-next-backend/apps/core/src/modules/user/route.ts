bd8f2c64e626d0c88b89d91efa459f5eaab1b1ed
Joabesv <joabevarjao123@gmail.com>
2024-03-24 17:39:17 -0300

    chore: deprecate old user

--- apps/ufabc-next-backend/apps/core/src/modules/user/route.ts
+++
@@ -1,8 +0,0 @@
-import { gradesUser } from './handlers/gradesUser.js';
-import type { FastifyInstance } from 'fastify';
-
-// eslint-disable-next-line require-await
-export async function userRoute(app: FastifyInstance) {
-  // TODO: deprecate
-  app.get('/grades', gradesUser);
-}
