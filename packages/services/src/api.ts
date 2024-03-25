17dc1c4c6aff205e59d76679de6b2b12dedae255
Joabesv <joabevarjao123@gmail.com>
2024-03-25 19:15:46 -0300

    chore: differentiate envs

--- packages/services/src/api.ts
+++ packages/services/src/api.ts
@@ -1,7 +1,16 @@
 import axios from 'axios';
 import { authStore } from 'stores';
 
-export const api = axios.create({ baseURL: 'https://api.ufabcnext.com/v1/' });
+const resolveEndpoint = (env?: string) =>
+  ({
+    development: 'http://localhost:8011/v1',
+    staging: 'https://api.ufabcnext.com/v1',
+    production: 'https://api.ufabcnext.com/v1',
+  })[env!] || 'https://api.ufabcnext.com/v1';
+
+export const api = axios.create({
+  baseURL: resolveEndpoint(process.env.NODE_ENV),
+});
 
 api.interceptors.request.use(async (config) => {
   const { token } = authStore.getState();
