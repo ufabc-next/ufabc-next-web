d517ccdf5cea65c5c8991587f3231b9705ff1e6f
Joabesv <joabevarjao123@gmail.com>
2024-04-08 11:50:58 -0300

    chore: remove constants package

--- apps/ufabc-next-backend/packages/constants/config.ts
+++
@@ -1,8 +0,0 @@
-export const MAILER_CONFIG = {
-  EMAIL_CONFIRMATION_TEMPLATE: 'Confirmation',
-  EMAIL_RECOVERY_TEMPLATE: 'Recovery',
-  EMAIL: 'contato@ufabcnext.com',
-} as const;
-
-export const WEB_URL_LOCAL = 'http://localhost:3000/' as const;
-export const WEB_URL = 'https://www.ufabcnext.com/app/' as const;
