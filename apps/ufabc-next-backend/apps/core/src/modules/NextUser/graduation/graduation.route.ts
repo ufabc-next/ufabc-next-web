a3bd70952adab281ff005ba3f7a5b5be2f965d9d
Joabesv <joabevarjao123@gmail.com>
2024-04-20 01:53:09 -0300

    feat: fetch by graduation

--- apps/ufabc-next-backend/apps/core/src/modules/NextUser/graduation/graduation.route.ts
+++ apps/ufabc-next-backend/apps/core/src/modules/NextUser/graduation/graduation.route.ts
@@ -1,11 +1,11 @@
-import { GraduationModel } from '@/models/Graduation.js';
-import { GraduationSubjectModel } from '@/models/GraduationSubject.js';
-import { admin } from '@/hooks/admin.js';
-import { authenticate } from '@/hooks/authenticate.js';
-import { GraduationRepository } from './graduation.repository.js';
-import { GraduationService } from './graduation.service.js';
-import { GraduationHandler } from './graduation.handlers.js';
-import type { FastifyInstance } from 'fastify';
+import { GraduationModel } from "@/models/Graduation.js";
+import { GraduationSubjectModel } from "@/models/GraduationSubject.js";
+import { admin } from "@/hooks/admin.js";
+import { authenticate } from "@/hooks/authenticate.js";
+import { GraduationRepository } from "./graduation.repository.js";
+import { GraduationService } from "./graduation.service.js";
+import { GraduationHandler } from "./graduation.handlers.js";
+import type { FastifyInstance } from "fastify";
 
 // eslint-disable-next-line require-await
 export async function graduationRoutes(app: FastifyInstance) {
@@ -16,16 +16,23 @@ export async function graduationRoutes(app: FastifyInstance) {
   const graduationService = new GraduationService(graduationRepository);
   const graduationHandler = new GraduationHandler(graduationService);
 
-  app.decorate('graduationService', graduationService);
+  app.decorate("graduationService", graduationService);
 
   app.get<{ Querystring: { limit: number } }>(
-    '/',
+    "/",
     { onRequest: [authenticate] },
     graduationHandler.listGraduations,
   );
   app.get(
-    '/subjects',
+    "/subjects",
     { onRequest: [authenticate, admin] },
     graduationHandler.listGraduationsSubjects,
+  );
+  app.get<{ Params: { graduationId: number }; Querystring: { limit: number } }>(
+    "/subjects/:graduationId",
+    // {
+    //   onRequest: [authenticate, admin],
+    // },
+    graduationHandler.listGraduationSubjectsByGraduationId,
   );
 }
--- apps/ufabc-next-backend/apps/core/src/modules/NextUser/graduation/graduation.service.ts
+++ apps/ufabc-next-backend/apps/core/src/modules/NextUser/graduation/graduation.service.ts
@@ -1,6 +1,7 @@
-import type { Graduation } from '@/models/Graduation.js';
-import type { GraduationRepository } from './graduation.repository.js';
-import type { FilterQuery } from 'mongoose';
+import { GraduationSubjectModel } from "@/models/GraduationSubject.js";
+import type { Graduation } from "@/models/Graduation.js";
+import type { GraduationRepository } from "./graduation.repository.js";
+import type { FilterQuery } from "mongoose";
 
 export class GraduationService {
   constructor(private readonly graduationRepository: GraduationRepository) {}
@@ -17,5 +18,17 @@ export class GraduationService {
     const graduationsSubject =
       await this.graduationRepository.findGraduationSubject({});
     return graduationsSubject;
+  }
+
+  async listGraduationSubjects(limit: number, graduationId: number) {
+    // fetch graduation based on a student graduation
+    const subjects = await GraduationSubjectModel.find({
+      graduation: graduationId,
+    })
+      .limit(limit)
+      .populate("subject", "name creditos -_id")
+      .lean({ virtuals: true });
+
+    return subjects;
   }
 }
