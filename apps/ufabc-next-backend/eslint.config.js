514a97ae52dd9b0ba28b5896fe502019a22df060
Joabesv <joabevarjao123@gmail.com>
2024-04-20 00:58:09 -0300

    feat: remove prettier

--- apps/ufabc-next-backend/eslint.config.js
+++ apps/ufabc-next-backend/eslint.config.js
@@ -10,5 +10,5 @@ export default eslintConfig(
       'unicorn/filename-case': ['off'],
     },
   },
-  { unocss: false, vue: false, prettier: true, markdown: true },
+  { unocss: false, vue: false, prettier: false, markdown: true },
 );
