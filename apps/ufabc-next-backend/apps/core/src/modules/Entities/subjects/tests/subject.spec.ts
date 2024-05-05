db4893df8f07c3718a9470a36ce29a1876693cad
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2024-05-05 19:46:24 -0300

    test: add testcontainers to subjects tests

--- apps/ufabc-next-backend/apps/core/src/modules/Entities/subjects/tests/subject.spec.ts
+++ apps/ufabc-next-backend/apps/core/src/modules/Entities/subjects/tests/subject.spec.ts
@@ -1,14 +1,30 @@
 import assert from 'node:assert/strict';
-import { describe, it } from 'node:test';
+import { after, before, describe, it } from 'node:test';
 import mongoose from 'mongoose';
+import { GenericContainer, Wait } from 'testcontainers';
 import { SubjectModel } from '@/models/Subject.js';
-import { Config } from '@/config/config.js';
 import { SubjectRepository } from '../subjects.repository.js';
 
 describe('Subjects repository unit tests', () => {
-  it('should create a subject', async () => {
-    mongoose.connect(Config.MONGODB_CONNECTION_URL);
+  let connection: typeof mongoose;
 
+  before(async () => {
+    const mongoContainer = await new GenericContainer('mongo:4.0.1')
+      .withExposedPorts(27017)
+      .withWaitStrategy(Wait.forLogMessage(/.*waiting for connections.*/i))
+      .start();
+
+    connection = await mongoose.connect(
+      `mongodb://127.0.0.1:${mongoContainer.getMappedPort(27017)}/`,
+    );
+  });
+
+  after(async () => {
+    //No need to stop the container, it will be stopped automatically
+    await connection.disconnect();
+  });
+
+  it('Should create a subject', async () => {
     const subjectRepository = new SubjectRepository(SubjectModel);
 
     const createdSubject = await subjectRepository.createSubject({
@@ -21,9 +37,6 @@ describe('Subjects repository unit tests', () => {
       name: 'Algebra Linear',
     });
 
-    //This should work if I use testcontainers to isolate the database
     assert.strictEqual(createdSubject.name, subjectInDb[0].name);
-
-    mongoose.disconnect();
   });
 });
