dfacdd8a253d73f0b1dfdbf042a310b82741756c
Joabesv <joabevarjao123@gmail.com>
2024-05-24 09:39:25 -0300

    feat: break functions in utils

--- apps/ufabc-next-extension/src/scripts/contentScriptSigaa.js
+++ apps/ufabc-next-extension/src/scripts/contentScriptSigaa.js
@@ -1,6 +1,7 @@
 import Toastify from "toastify-js";
 import "toastify-js/src/toastify.css";
 import Utils from "../utils/extensionUtils";
+import {  normalizeDiacritics, scrapeGradesConsulting } from "../utils/sigaa";
 Utils.injectStyle("styles/portal.css");
 
 const loading = require("../images/loading.svg");
@@ -8,102 +9,17 @@ const errorSVG = require("../images/error.svg");
 const logoWhite = require("../images/logo-white.svg");
 
 const trs = document.querySelectorAll("#agenda-docente tbody tr");
-const keypair = Array.from(trs).map((line) =>
+const tablesRowsArray = Array.from(trs);
+const rawStudentInfo = tablesRowsArray.map((line) =>
 	Array.from(line.children).map((column) =>
-		normalizeDiacritcs(column.innerText),
+		normalizeDiacritics(column.innerText),
 	),
 );
 
-const completeObject = Object.fromEntries(keypair);
+const studentInfo = Object.fromEntries(rawStudentInfo);
 
-function normalizeDiacritcs(stringElement) {
-	return stringElement
-		.trim()
-		.normalize("NFD")
-		.replace(/[\u0300-\u036f]/g, "")
-		.replace(/[-:]/g, "")
-		.toLocaleLowerCase();
-}
-
-function isIndexSigaa() {
-	return (
-		document.location.href.indexOf(
-			"https://sig.ufabc.edu.br/sigaa/portais/discente/discente.jsf",
-		) !== -1
-	);
-}
-
-function getValuesFromGetGradesPageSigaa() {
-	const updateDatetimeHTML = document
-		.querySelector(".dataAtual")
-		.textContent.trim();
-
-	const [day, month, year, hour, minute] = updateDatetimeHTML.match(/\d+/g);
-	const updateDatetime = new Date(year, month - 1, day, hour, minute);
-
-	// * dados do aluno
-	const studentDataHTML = document.querySelector(
-		"#identificacao > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(1) > td",
-	).textContent;
-	const courseHTML = document.querySelector(
-		"#identificacao > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(2) > td",
-	);
-
-	const [nameStudent, ra] = studentDataHTML.trim().split(" - ");
-	const studentCourse = courseHTML.innerText;
-
-	const quadsTableElement = document.querySelectorAll(".tabelaRelatorio");
-	const quadsTable = Array.from(quadsTableElement);
-
-	const studentHistory = [];
-	for (const table of quadsTable) {
-		const studentseason = table.querySelector("caption");
-		const [year, quad] = studentseason.textContent.trim().split(".");
-
-		const tableHeadersElement = table.querySelectorAll("th");
-		const tableRowsElement = table.querySelectorAll("tbody > tr");
-		const rawTableHeaders = Array.from(tableHeadersElement);
-
-		const wantedFields = ["codigo", "disciplina", "resultado", "situacao"];
-		const indexWantedFields = [];
-		const tableHeaders = rawTableHeaders.filter((rawItem, index) => {
-			const item = normalizeDiacritcs(rawItem.innerText);
-
-			if (wantedFields.includes(item)) indexWantedFields.push(index);
-			return wantedFields.includes(item);
-		});
-
-		const tableRows = Array.from(tableRowsElement);
-		for (const row of tableRows) {
-			const rowChildrens = Array.from(row.children);
-			const cells = rowChildrens
-				.filter((cellHTML, index) => indexWantedFields.includes(index))
-				.map((cellHTML) => cellHTML.innerText); // picking exact header positions
-
-			const disciplina = {};
-
-			cells.forEach((item, index) => {
-				const normalizedHeaderText = normalizeDiacritcs(
-					tableHeaders[index].textContent,
-				);
-				disciplina[normalizedHeaderText] = item;
-			});
-			disciplina["ano"] = year;
-			disciplina["periodo"] = quad;
-
-			studentHistory.push(disciplina);
-		}
-	}
-
-	const result = {
-		updateTime: updateDatetime,
-		curso: studentCourse,
-		ra: Number(ra),
-		disciplinas: studentHistory,
-	};
-
-	return result;
-}
+const sigaaURL = new URL(document.location.href);
+const isDiscentesPath = sigaaURL.pathname.includes("discente.jsf");
 
 const toast = () => {
 	const name = JSON.parse(localStorage.getItem("name"))["email"];
@@ -129,12 +45,12 @@ const toast = () => {
 	});
 };
 
-if (isIndexSigaa()) {
+if (isDiscentesPath) {
 	const observer = new MutationObserver((list) => {
 		if (document.contains(document.querySelector(".notas"))) {
-			console.log("userInfo", localStorage.getItem("name"));
-			const result = getValuesFromGetGradesPageSigaa();
-			localStorage.setItem("userGraduationInfo", JSON.stringify(result));
+			console.log("local", localStorage.getItem("name"));
+			const result = scrapeGradesConsulting();
+			localStorage.setItem("name", JSON.stringify(result));
 
 			console.log("It's in the DOM!");
 			toast().showToast();
@@ -144,9 +60,9 @@ if (isIndexSigaa()) {
 
 	const newObserver = new MutationObserver((list) => {
 		if (document.contains(document.querySelector("#agenda-docente"))) {
-			localStorage.setItem("name", JSON.stringify(completeObject));
+			localStorage.setItem("name", JSON.stringify(studentInfo));
 			console.log("It's in the DOM!");
-			toast.showToast();
+			toast().showToast();
 			newObserver.disconnect();
 		}
 	});
---
+++ apps/ufabc-next-extension/src/utils/sigaa/index.js
@@ -0,0 +1,89 @@
+function normalizeDiacritics(stringElement) {
+	return stringElement
+		.trim()
+		.normalize("NFD")
+		.replace(/[\u0300-\u036f]/g, "")
+		.replace(/[-:]/g, "")
+		.toLocaleLowerCase();
+}
+
+function scrapeGradesConsulting() {
+	const sigaaUpdatetime = document
+		.querySelector(".dataAtual")
+		.textContent.trim();
+
+	const [day, month, year, hour, minute] = sigaaUpdatetime.match(/\d+/g);
+	const updateDatetime = new Date(year, month - 1, day, hour, minute);
+
+	// * dados do aluno
+	const studentDataHTML = document.querySelector(
+		"#identificacao > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(1) > td",
+	).textContent;
+	const courseHTML = document.querySelector(
+		"#identificacao > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(2) > td",
+	);
+	const studentCourse = courseHTML.innerText;
+	const [, ra] = studentDataHTML.trim().split(" - ");
+
+	const quadsTableElement = document.querySelectorAll(".tabelaRelatorio");
+	const quadsTable = Array.from(quadsTableElement);
+
+	const studentDisciplinaHistory = [];
+	for (const tableElement of quadsTable) {
+		const studentseason = tableElement.querySelector("caption");
+		const [year, quad] = studentseason.textContent.trim().split(".");
+
+		const tableHeadersElement = tableElement.querySelectorAll("th");
+		const tableRowsElement = tableElement.querySelectorAll("tbody > tr");
+		const rawTableHeaders = Array.from(tableHeadersElement);
+
+		const wantedFields = ["codigo", "disciplina", "resultado", "situacao"];
+		const indexWantedFields = [];
+		const tableHeaders = rawTableHeaders.filter((rawItem, index) => {
+			const item = normalizeDiacritics(rawItem.innerText);
+
+			if (wantedFields.includes(item)) {
+				// crimes
+        indexWantedFields.push(index);
+			}
+
+			return wantedFields.includes(item);
+		});
+
+		const tableRows = Array.from(tableRowsElement);
+		for (const row of tableRows) {
+			const rowChildrens = Array.from(row.children);
+			const cells = rowChildrens
+				.filter((_, index) => indexWantedFields.includes(index))
+				.map((cellHTML) => cellHTML.innerText); // picking exact header positions
+
+			const disciplina = {};
+
+			cells.forEach((item, index) => {
+				const normalizedHeaderText = normalizeDiacritics(
+					tableHeaders[index].textContent,
+				);
+				disciplina[normalizedHeaderText] = item;
+			});
+
+			disciplina.ano = year;
+			disciplina.periodo = quad;
+
+			studentDisciplinaHistory.push(disciplina);
+		}
+	}
+
+	const userHistory = {
+		updateTime: updateDatetime,
+		curso: studentCourse,
+		ra: Number(ra),
+		disciplinas: studentDisciplinaHistory,
+	};
+
+	return userHistory;
+}
+
+module.exports = {
+	normalizeDiacritics,
+	scrapeGradesConsulting,
+};
