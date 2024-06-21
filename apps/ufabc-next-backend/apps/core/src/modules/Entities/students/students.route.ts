21f425793403c6ae07bb73d2a87b0d044b450098
Joabesv <joabevarjao123@gmail.com>
2024-06-21 11:46:50 -0300

    feat: return user course and ids

--- apps/ufabc-next-backend/apps/core/src/modules/Entities/students/students.route.ts
+++ apps/ufabc-next-backend/apps/core/src/modules/Entities/students/students.route.ts
@@ -4,7 +4,11 @@ import { GraduationHistoryModel } from '@/models/GraduationHistory.js';
 import { StudentModel } from '@/models/Student.js';
 import { StudentHandler } from './students.handlers.js';
 import { StudentRepository } from './students.repository.js';
-import { createStudentSchema, listSeasonStudentSchema, studentDisciplinasStatsSchema } from './students.schema.js';
+import {
+  createStudentSchema,
+  listSeasonStudentSchema,
+  studentDisciplinasStatsSchema,
+} from './students.schema.js';
 import { StudentService } from './students.service.js';
 import type { FastifyInstance } from 'fastify';
 
@@ -20,18 +24,19 @@ export async function studentsRoute(app: FastifyInstance) {
   const studentHandler = new StudentHandler(studentService);
 
   app.post(
-    "/student",
+    '/student',
     { schema: createStudentSchema },
     studentHandler.createStudent,
   );
   app.get(
-    "/student/studentId/",
+    '/student/studentId/',
     { schema: listSeasonStudentSchema, onRequest: [authenticate] },
     studentHandler.listSeasonStudent,
   );
   app.get(
-    "/student/stats/disciplinas",
+    '/student/stats/disciplinas',
     { schema: studentDisciplinasStatsSchema },
     studentHandler.studentDisciplinasStats,
   );
+  app.get('/student/courses', studentHandler.courseinfo);
 }
