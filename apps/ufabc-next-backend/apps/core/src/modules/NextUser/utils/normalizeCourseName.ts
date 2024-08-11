1e382f8870a33c3dc31f607185560690e3b972fd
Joabesv <joabevarjao123@gmail.com>
2024-08-11 10:19:47 -0300

    refac: remove unused files

--- apps/ufabc-next-backend/apps/core/src/modules/NextUser/utils/normalizeCourseName.ts
+++
@@ -1,7 +0,0 @@
-export function normalizeCourseName(name: string) {
-  if (name === 'Bacharelado em CIências e Humanidades') {
-    const validName = 'Bacharelado em Ciências e Humanidades';
-    return validName;
-  }
-  return name;
-}
