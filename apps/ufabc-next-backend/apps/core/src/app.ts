c189805f4742084da4da90a65cd7bee38f4b0a2b
Joabesv <joabevarjao123@gmail.com>
2024-09-21 10:36:58 -0300

    remove unused file

--- apps/ufabc-next-backend/apps/core/src/app.ts
+++ apps/ufabc-next-backend/apps/core/src/app.ts
@@ -5,7 +5,6 @@ import { publicModule } from './modules/public/public.module.js';
 import { userModule } from './modules/user/user.module.js';
 import { syncModule } from './modules/sync/sync.module.js';
 import { backOfficeModule } from './modules/backoffice/backoffice.module.js';
-import Oauth2Debug from './plugins/oauth2/oauthFixed.js';
 import { nextJobs } from './queue/NextJobs.js';
 import { nextWorker } from './queue/NextWorker.js';
 import { connect } from 'mongoose';
