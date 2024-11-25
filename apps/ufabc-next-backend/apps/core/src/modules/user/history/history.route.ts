76382eda38bba9e67e25de4b6fd2d1e5fcdf7320
Joabesv <joabevarjao123@gmail.com>
2024-11-24 23:00:31 -0300

    fix: route

--- apps/ufabc-next-backend/apps/core/src/modules/user/history/history.route.ts
+++ apps/ufabc-next-backend/apps/core/src/modules/user/history/history.route.ts
@@ -5,18 +5,21 @@ import { seasonCourses } from './handlers/seasonCourses.js';
 import type { FastifyInstance, preHandlerAsyncHookHandler } from 'fastify';
 import { HistoryModel } from '@/models/History.js';
 
-const sigHistoryPreHandler: preHandlerAsyncHookHandler = async (request, reply) => {
+const sigHistoryPreHandler: preHandlerAsyncHookHandler = async (
+  request,
+  reply,
+) => {
   // content-script request assumes the page as hostname
-  if (request.hostname !== 'sig.ufabc.edu.br') {
-    return reply.badRequest()
-  }
 
-  request.log.warn({
-    ip: request.ip,
-    userAgent: request.headers['user-agent']
-  }, 'requestedBy')
+  request.log.warn(
+    {
+      ip: request.ip,
+      userAgent: request.headers['user-agent'],
+    },
+    'requestedBy',
+  );
   return;
-}
+};
 
 export async function historyRoutes(app: FastifyInstance) {
   app.get(
@@ -26,12 +29,19 @@ export async function historyRoutes(app: FastifyInstance) {
   );
 
   app.post('/sigaa', createHistory);
-  app.get('/me', { preHandler: sigHistoryPreHandler }, async (request, reply) => {
-    const { ra } = request.query as { ra: number }
-    const history = await HistoryModel.findOne({ ra }, { curso: 1, grade: 1, ra: 1, _id: 0 }).lean<{ curso: string; grade: string; ra: number }>();
-    if (!history) {
-      return reply.badRequest('User History not found')
-    }
-    return history;
-  })
+  app.get(
+    '/me',
+    { preHandler: sigHistoryPreHandler },
+    async (request, reply) => {
+      const { ra } = request.query as { ra: number };
+      const history = await HistoryModel.findOne(
+        { ra },
+        { curso: 1, grade: 1, ra: 1, _id: 0 },
+      ).lean<{ curso: string; grade: string; ra: number }>();
+      if (!history) {
+        return reply.badRequest('User History not found');
+      }
+      return history;
+    },
+  );
 }
