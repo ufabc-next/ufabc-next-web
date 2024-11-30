d9a6c2edd9797d892b635b66a98ef9f640e5d3a0
Joabesv <joabevarjao123@gmail.com>
2024-11-30 00:59:20 -0300

    remove old code

--- apps/ufabc-next-backend/apps/core/src/modules/user/user.module.ts
+++ apps/ufabc-next-backend/apps/core/src/modules/user/user.module.ts
@@ -1,12 +1,8 @@
 import { commentRoute } from './comments/comments.route.js';
 import { courseStatsRoute } from './courseStats/courseStats.route.js';
-import { graduationRoutes } from './graduation/graduation.route.js';
 import type { FastifyInstance } from 'fastify';
 
 export async function userModule(app: FastifyInstance) {
-  await app.register(graduationRoutes, {
-    prefix: '/graduations',
-  });
   await app.register(courseStatsRoute, {
     prefix: '/courseStats',
   });
