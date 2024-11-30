913e65d8004901ff1383cb6fd399d7a772ada3b0
Joabesv <joabevarjao123@gmail.com>
2024-11-30 12:58:28 -0300

    remove tsup

--- apps/ufabc-next-backend/apps/core/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup';
-
-export default defineConfig({
-  entry: ['src/**/*.ts'],
-  format: 'esm',
-  clean: true,
-  splitting: false,
-  tsconfig: './tsconfig.json',
-});
