d1380205dfcdea1aaa7f022e483638b51b835b4f
Mateus Braga <teteu040506@gmail.com>
2025-04-19 23:26:18 -0300

    chore: webpack to vite

---
+++ apps/container/vite.config.ts
@@ -0,0 +1,28 @@
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+import { fileURLToPath } from 'node:url';
+
+export default defineConfig({
+  server: {
+    port: 3000,
+    host: true,
+    strictPort: true,
+  },
+  build: {
+    outDir: 'dist',
+  },
+  preview: {
+    port: 3000,
+    host: true,
+    strictPort: true,
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    ],
+  },
+});
