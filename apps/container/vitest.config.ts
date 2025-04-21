6efef3082369e13d3a5dedef0fc258014a87ed32
Mateus Braga <teteu040506@gmail.com>
2025-04-20 23:44:32 -0300

    finally build

--- apps/container/vitest.config.ts
+++ apps/container/vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue() as any],
   test: {
     globals: true,
     environment: 'jsdom',
