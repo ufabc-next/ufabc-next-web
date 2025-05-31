1b2f4cc29cc062ad0275a5fb5fb70812c18917b2
Mateus Braga <teteu040506@gmail.com>
2025-05-31 00:15:16 -0300

    wip: eslint custom package

---
+++ packages/eslint-config-custom/base.js
@@ -0,0 +1,45 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import turboPlugin from 'eslint-plugin-turbo';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import unusedImports from 'eslint-plugin-unused-imports';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  js.configs.recommended,
+  eslintConfigPrettier,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      turbo: turboPlugin,
+    },
+    rules: {
+      'turbo/no-undeclared-env-vars': 'warn',
+    },
+  },
+  {
+    plugins: {
+      import: importPlugin,
+      'simple-import-sort': simpleImportSort,
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+  {
+    ignores: ['/node_modules/**', '/dist/**', '/build/**', '/coverage/**'],
+  },
+]);
