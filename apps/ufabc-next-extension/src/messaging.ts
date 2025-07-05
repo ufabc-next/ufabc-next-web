2720a337b96db860ceed2e689bf2eb71fa85e268
AlencarLima <carlos.alencar.99@gmail.com>
2025-07-05 16:48:01 -0300

    feat: add getTokenMoodle method to ProtocolMap for Moodle token retrieval

--- apps/ufabc-next-extension/src/messaging.ts
+++ apps/ufabc-next-extension/src/messaging.ts
@@ -4,6 +4,7 @@ import type { Cookies } from 'wxt/browser';
 interface ProtocolMap {
   getToken({ action, pageURL }: { action: 'getToken', pageURL: string }): Cookies.Cookie | null;
   getTokenMatricula({ action, pageURL }: { action: 'getTokenMatricula', pageURL: string }): Cookies.Cookie | null
+  getTokenMoodle({ action, pageURL }: { action: 'getTokenMoodle', pageURL: string }): Cookies.Cookie | null;
 }
 
 export const { sendMessage, onMessage } = defineExtensionMessaging<ProtocolMap>();
