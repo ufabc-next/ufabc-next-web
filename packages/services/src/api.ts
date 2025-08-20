9715b5b9ac511f397e94e536a4c7ee53e2dcaa8a
Rafael G. <rafaelguerrae@outlook.com>
2025-08-20 10:56:38 -0300

    fix(api): update baseURL to use environment var

--- packages/services/src/api.ts
+++ packages/services/src/api.ts
@@ -9,7 +9,7 @@ const resolveEndpoint = (env?: string) =>
   })[env!] || 'https://api.v2.ufabcnext.com';
 
 export const api = axios.create({
-  baseURL: resolveEndpoint('production'),
+  baseURL: resolveEndpoint(process.env.VITE_APP_ENV),
 });
 
 api.interceptors.request.use(async (config) => {
