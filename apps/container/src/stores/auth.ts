8197964dea24c24022ae0df3a4c876de8bf5361d
Mateus Braga <teteu040506@gmail.com>
2025-09-01 23:42:24 -0300

    wip

---
+++ apps/container/src/stores/auth.ts
@@ -0,0 +1,34 @@
+import { User } from '@ufabc-next/types';
+import { defineStore } from 'pinia';
+
+type AuthState = {
+  user: User | null;
+  token: string | null;
+};
+
+export const useAuthStore = defineStore('auth', {
+  persist: true,
+  state: (): AuthState => {
+    return {
+      user: null,
+      token: null,
+    };
+  },
+  getters: {
+    isLoggedIn: (state): boolean => Boolean(state.user),
+  },
+  actions: {
+    authenticate(token: string) {
+      if (token) {
+        const user = JSON.parse(atob(token.split('.')[1])) as User;
+        this.token = token;
+        this.user = user;
+      }
+    },
+    logOut() {
+      this.user = null;
+      this.token = null;
+      window.location.href = '/';
+    },
+  },
+});
