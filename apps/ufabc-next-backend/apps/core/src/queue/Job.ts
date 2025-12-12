ce65d4cc45058c972000b6f38acc1c0d8c26245e
grecoww <132113259+grecoww@users.noreply.github.com>
2025-12-11 22:03:53 -0300

    fix: ensure apropriatte 303 code in redirect

--- apps/ufabc-next-backend/apps/core/src/queue/Job.ts
+++ apps/ufabc-next-backend/apps/core/src/queue/Job.ts
@@ -222,7 +222,7 @@ export class Jobs implements JobImpl {
               maxAge: 3600,
               sameSite: 'lax',
             });
-            return reply.redirect('/board/ui');
+            return reply.redirect('/board/ui', 303);
           }
           await request.jwtVerify({
             //@ts-expect-error extractToken property not well defined in jwt type definitions
