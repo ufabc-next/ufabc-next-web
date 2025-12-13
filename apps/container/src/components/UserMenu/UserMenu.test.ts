9488ac3fa1f3ffda769482c9dcfdb3bd419544b2
Rafael Evangelista <rafaelguerrae@outlook.com>
2025-12-13 15:59:06 -0300

    chore: removing test

--- apps/container/src/components/UserMenu/UserMenu.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, test } from 'vitest';
-
-import UserMenu from './UserMenu.vue';
-
-describe('<UserMenu />', () => {
-  test('renders component', () => {
-    expect(UserMenu).toBeTruthy();
-  });
-});
