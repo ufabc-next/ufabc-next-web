90a872a3fa6e49a537e2f954130d5b04319f278b
Rafael Evangelista <rafaelguerrae@outlook.com>
2025-12-13 15:55:33 -0300

    chore: organizing themes and adding ternary

--- apps/container/src/theme.ts
+++ apps/container/src/theme.ts
@@ -39,54 +39,103 @@ export const darkTheme: ThemeDefinition = {
   },
 };
 
-export function applyChartsTheme() {
-  const isDarkMode = document.body.classList.contains('highcharts-dark');
-
-  Highcharts.setOptions({
-    chart: {
-      backgroundColor: isDarkMode ? '#212121' : '#ffffff',
+const darkMode = {
+  chart: {
+    backgroundColor: '#212121',
+  },
+  title: {
+    style: {
+      color: '#E0E0E3',
     },
+  },
+  yAxis: {
     title: {
       style: {
-        color: isDarkMode ? '#E0E0E3' : '#333333',
+        color: '#E0E0E3',
       },
     },
-    yAxis: {
-      title: {
-        style: {
-          color: isDarkMode ? '#E0E0E3' : '#333333',
-        },
-      },
-      labels: {
-        style: {
-          color: isDarkMode ? '#E0E0E3' : '#333333',
-        },
-      },
-    },
-    xAxis: {
-      title: {
-        style: {
-          color: isDarkMode ? '#E0E0E3' : '#333333',
-        },
-      },
-      labels: {
-        style: {
-          color: isDarkMode ? '#E0E0E3' : '#333333',
-        },
-      },
-    },
-    legend: {
-      itemStyle: {
-        color: isDarkMode ? '#E0E0E3' : '#333333',
-      },
-      itemHoverStyle: {
-        color: isDarkMode ? '#FFF' : '#000000',
-      },
-    },
-    credits: {
+    labels: {
       style: {
-        color: isDarkMode ? '#1f1f1f' : '#ffffff',
+        color: '#E0E0E3',
       },
     },
-  });
+  },
+  xAxis: {
+    title: {
+      style: {
+        color: '#E0E0E3',
+      },
+    },
+    labels: {
+      style: {
+        color: '#E0E0E3',
+      },
+    },
+  },
+  legend: {
+    itemStyle: {
+      color: '#E0E0E3',
+    },
+    itemHoverStyle: {
+      color: '#FFF',
+    },
+  },
+  credits: {
+    style: {
+      color: '#1f1f1f',
+    },
+  },
+};
+
+const lightMode = {
+  chart: {
+    backgroundColor: '#ffffff',
+  },
+  title: {
+    style: {
+      color: '#333333',
+    },
+  },
+  yAxis: {
+    title: {
+      style: {
+        color: '#333333',
+      },
+    },
+    labels: {
+      style: {
+        color: '#333333',
+      },
+    },
+  },
+  xAxis: {
+    title: {
+      style: {
+        color: '#333333',
+      },
+    },
+    labels: {
+      style: {
+        color: '#333333',
+      },
+    },
+  },
+  legend: {
+    itemStyle: {
+      color: '#333333',
+    },
+    itemHoverStyle: {
+      color: '#000000',
+    },
+  },
+  credits: {
+    style: {
+      color: '#ffffff',
+    },
+  },
+};
+
+export function applyChartsTheme() {
+  const isDarkMode = document.body.classList.contains('highcharts-dark');
+  Highcharts.setOptions(isDarkMode ? darkMode : lightMode);
 }
