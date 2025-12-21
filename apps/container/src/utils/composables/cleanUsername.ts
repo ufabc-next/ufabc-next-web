92b9ac3dcb9f6d75199506aafb6fdb5b574ad147
grecoww <132113259+grecoww@users.noreply.github.com>
2025-12-20 23:03:27 -0300

    fix: some points from reviews

--- apps/container/src/utils/composables/cleanUsername.ts
+++ apps/container/src/utils/composables/cleanUsername.ts
@@ -1,14 +1,12 @@
-import { ref } from 'vue';
+import { computed } from 'vue';
 
 import { useAuthStore } from '@/stores/auth';
 
 export function useCleanUsername() {
   const authStore = useAuthStore();
 
-  const userCleanUsername = ref('');
-
-  userCleanUsername.value =
-    authStore.user?.email?.replace('@aluno.ufabc.edu.br', '') || '';
-
+  const userCleanUsername = computed(
+    () => authStore.user?.email?.replace('@aluno.ufabc.edu.br', '') || '',
+  );
   return userCleanUsername;
 }
