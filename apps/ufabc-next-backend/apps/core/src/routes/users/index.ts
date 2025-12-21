ca95f339402e2261eb822e48ecaa8d08dbfd7a5e
grecoww <132113259+grecoww@users.noreply.github.com>
2025-12-20 23:18:53 -0300

    fix: improve error typing

--- apps/ufabc-next-backend/apps/core/src/routes/users/index.ts
+++ apps/ufabc-next-backend/apps/core/src/routes/users/index.ts
@@ -138,20 +138,21 @@ const plugin: FastifyPluginAsyncZodOpenApi = async (app) => {
 
       try {
         await validateUserData(email, ra);
-      } catch (err: any) {
-        switch (err.message) {
-          case 'RA_NOT_FOUND':
-            return reply.badRequest('O RA digitado não existe.');
-          case 'HAS_UFABC_CONTRACT':
-            return reply.forbidden(
-              'O aluno não pode ter contrato com a UFABC.',
-            );
-          case 'INVALID_EMAIL':
-            return reply.forbidden('O email fornecido não é válido.');
-          default:
-            request.log.error({ err }, 'unexpected validation error');
-            return reply.internalServerError('Erro de validação inesperado');
-        }
+      } catch (err: unknown) {
+        if (err instanceof Error)
+          switch (err.message) {
+            case 'RA_NOT_FOUND':
+              return reply.badRequest('O RA digitado não existe.');
+            case 'HAS_UFABC_CONTRACT':
+              return reply.forbidden(
+                'O aluno não pode ter contrato com a UFABC.',
+              );
+            case 'INVALID_EMAIL':
+              return reply.forbidden('O email fornecido não é válido.');
+            default:
+              request.log.error({ err }, 'unexpected validation error');
+              return reply.internalServerError('Erro de validação inesperado');
+          }
       }
 
       try {
