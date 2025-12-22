227a70d5f1a0a0e3446ddd3e08cf3b5b48370a5f
Joabesv <joabevarjao123@gmail.com>
2025-12-22 09:02:14 -0300

    wip: moodle route

--- apps/ufabc-next-backend/apps/core/src/controllers/components-controller.ts
+++ apps/ufabc-next-backend/apps/core/src/controllers/components-controller.ts
@@ -1,3 +1,4 @@
+import { moodleSession } from '@/hooks/moodle-session.js';
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod';
 import { z } from 'zod';
 
@@ -5,6 +6,7 @@ const componentsController: FastifyPluginAsyncZod = async (app) => {
   app.route({
     method: 'GET',
     url: '/components/archives',
+    preHandler: [moodleSession],
     schema: {
       querystring: z.object({
         page: z.coerce.number().int().positive().optional(),
@@ -15,6 +17,10 @@ const componentsController: FastifyPluginAsyncZod = async (app) => {
           page: z.number().optional(),
         }),
       },
+      headers: z.object({
+        'session-id': z.string(),
+        'sess-key': z.string(),
+      }),
     },
     handler: async (request) => {
       const { page } = request.query;
