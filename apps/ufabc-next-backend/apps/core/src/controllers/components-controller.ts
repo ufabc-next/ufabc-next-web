229b5033abb3e9d79cd921b258bb099d991ef9ce
Joabesv <joabevarjao123@gmail.com>
2025-12-24 18:55:49 -0300

    cached requests

--- apps/ufabc-next-backend/apps/core/src/controllers/components-controller.ts
+++ apps/ufabc-next-backend/apps/core/src/controllers/components-controller.ts
@@ -4,10 +4,20 @@ import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod';
 import { z } from 'zod';
 import { getComponentArchives } from '@/services/components-service.js';
 import { JOB_NAMES } from '@/constants.js';
+import LRUWeakCache from 'lru-weak-cache';
 
 const moodleConnector = new MoodleConnector();
 
+const cache = new LRUWeakCache<{
+  session: { sessionId: string; sessKey: string };
+}>({
+  capacity: 10,
+  maxAge: 1000 * 60 * 60 * 24, // 24 Hours,
+});
+
 const componentsController: FastifyPluginAsyncZod = async (app) => {
+  app.decorate('cacheV2', cache);
+
   app.route({
     method: 'POST',
     url: '/components/archives',
@@ -24,7 +34,17 @@ const componentsController: FastifyPluginAsyncZod = async (app) => {
       }),
     },
     handler: async (request, reply) => {
+      const cache =
+        app.getDecorator<
+          LRUWeakCache<{ session: { sessionId: string; sessKey: string } }>
+        >('cacheV2');
       const session = request.requestContext.get('moodleSession')!;
+      const cachedSession = cache.get('session');
+      if (cachedSession) {
+        return reply.status(202).send({
+          status: 'success',
+        });
+      }
       const courses = await moodleConnector.getComponents(
         session.sessionId,
         session.sessKey,
@@ -35,24 +55,18 @@ const componentsController: FastifyPluginAsyncZod = async (app) => {
         return reply.internalServerError(componentArchives.error ?? 'No data');
       }
 
-      await app.manager.dispatch(JOB_NAMES.COMPONENTS_ARCHIVES_PROCESSING, {
-        component: componentArchives.data,
-        globalTraceId: request.id,
+      // await app.manager.dispatch(JOB_NAMES.COMPONENTS_ARCHIVES_PROCESSING, {
+      //   component: componentArchives.data,
+      //   globalTraceId: request.id,
+      //   session,
+      // });
+
+      cache.set('session', {
         session,
       });
-
       return reply.status(202).send({
         status: 'success',
       });
-    },
-  });
-
-  app.route({
-    method: 'GET',
-    url: '/components/archives/pdfs',
-    handler: async (request, reply) => {
-      const pdfs = await app.aws.s3.list(app.config.AWS_BUCKET ?? '');
-      return reply.status(200).send(pdfs.Contents);
     },
   });
 };
