0d7c57013fd626eea2b3081fa4a3b9eb5e1f7c66
Joabesv <joabevarjao123@gmail.com>
2026-01-19 21:24:03 -0300

    fix: boolean

--- apps/ufabc-next-backend/apps/core/src/lib/aws.service.ts
+++ apps/ufabc-next-backend/apps/core/src/lib/aws.service.ts
@@ -5,7 +5,7 @@ const LOCALSTACK_URL = 'http://localhost:4566';
 
 export const sesClient = new SESClient({
   region: process.env.AWS_REGION,
-  endpoint: process.env.USE_LOCALSTACK ? LOCALSTACK_URL : undefined,
+  endpoint: process.env.NODE_ENV !== 'prod' ? LOCALSTACK_URL : undefined,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
@@ -14,7 +14,7 @@ export const sesClient = new SESClient({
 
 export const s3Client = new S3Client({
   region: process.env.AWS_REGION,
-  endpoint: process.env.USE_LOCALSTACK ? LOCALSTACK_URL : undefined,
+  endpoint: process.env.NODE_ENV !== 'prod' ? LOCALSTACK_URL : undefined,
   forcePathStyle: process.env.USE_LOCALSTACK === 'true', // Required for LocalStack S3
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
