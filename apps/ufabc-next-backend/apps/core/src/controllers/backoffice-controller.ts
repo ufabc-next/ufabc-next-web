a5de4241978fb86ed10bac281a35d22558d07cd6
Joabesv <joabevarjao123@gmail.com>
2026-04-11 12:48:04 -0300

    delete endpoint

--- apps/ufabc-next-backend/apps/core/src/controllers/backoffice-controller.ts
+++ apps/ufabc-next-backend/apps/core/src/controllers/backoffice-controller.ts
@@ -2,9 +2,7 @@ import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod';
 
 import { z } from 'zod';
 
-import { UfabcParserConnector } from '@/connectors/ufabc-parser.js';
 import { adminHook } from '@/hooks/admin.js';
-import { TeacherModel } from '@/models/Teacher.js';
 
 const backofficeController: FastifyPluginAsyncZod = async (app) => {
   app.route({
@@ -47,17 +45,6 @@ const backofficeController: FastifyPluginAsyncZod = async (app) => {
       await app.redis.del(key);
       return reply.status(200).send({ message: 'Key deleted' });
     },
-  });
-
-  app.post('/teachers/repopulate', async (request, reply) => {
-    const teachersModel = await TeacherModel.find();
-    const teachers = teachersModel.map((teacher) => teacher.name);
-    const ufabcParserConnector = new UfabcParserConnector(request.id);
-    await ufabcParserConnector.sendTeachers(teachers);
-    return reply.status(200).send({
-      count: teachers.length,
-      message: 'starting repopulate',
-    });
   });
 };
 
